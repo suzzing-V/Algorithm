0d2e9d1306759abb0528d07afa8795b1b2133792
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-01-10 01:42:01 +0900

    첫번째 도전

--- b1018/src/Main.java
+++ b1018/src/Main.java
@@ -23,30 +23,89 @@ public class Main {
         }
 
         int count;
+        int changeCount;
         int min = 64;
-        for(int i = 0; i <= m - 8; i++) {
-            for(int j = 0; j <= n - 8; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(i + 7 < n && j + 7 < m) {
                 count = 0;
-                if(chess[i][j] == "b") {
-                    for(int k = i; k <= i + 7; k+=2) {
-                        for(int l = j; l <= j + 7; l+=2) {
-                            if(chess[k][l] == "w") {
-                                count++;
+                changeCount = 0;
+                if(chess[i][j].equals("B")) {
+                    for(int k = i; k < i + 8; k++) {
+                        for(int l = j; l < j + 8; l++) {
+                            if(((i + 7 - k) % 2 != 0 && (j + 7 - l) % 2 != 0)
+                            || ((i + 7 - k) % 2 == 0 && (j + 7 - l) % 2 == 0)) {
+                                if(chess[k][l].equals("W")) {
+                                    count++;
+                                }
+                            } else {
+                                if(chess[k][l].equals("B")) {
+                                    count++;
+                                }
                             }
                         }
                     }
 
-                    for(int k = i + 1; k <= i + 7; k+=2) {
-                        for(int l = j + 1; l <= j + 7; l+=2) {
-                            if(chess[k][l] == "w") {
-                                count++;
+                    for(int k = i; k < i + 8; k++) {
+                        for(int l = j; l < j + 8; l++) {
+                            if(((i + 7 - k) % 2 != 0 && (j + 7 - l) % 2 != 0)
+                            || ((i + 7 - k) % 2 == 0 && (j + 7 - l) % 2 == 0)) {
+                                if(chess[k][l].equals("B")) {
+                                    changeCount++;
+                                }
+                            } else {
+                                if(chess[k][l].equals("W")) {
+                                    changeCount++;
+                                }
                             }
                         }
+                    }
+
+                    if(changeCount < count) {
+                        count = changeCount;
                     }
                 } else {
+                    for(int k = i; k < i + 8; k++) {
+                        for(int l = j; l < j + 8; l++) {
+                            if(((i + 7 - k) % 2 != 0 && (j + 7 - l) % 2 != 0)
+                            || ((i + 7 - k) % 2 == 0 && (j + 7 - l) % 2 == 0)) {
+                                if(chess[k][l].equals("B")) {
+                                    count++;
+                                }
+                            } else {
+                                if(chess[k][l].equals("W")) {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
 
+                    for(int k = i; k < i + 8; k++) {
+                        for(int l = j; l < j + 8; l++) {
+                            if(((i + 7 - k) % 2 != 0 && (j + 7 - l) % 2 != 0)
+                            || ((i + 7 - k) % 2 == 0 && (j + 7 - l) % 2 == 0)) {
+                                if(chess[k][l].equals("W")) {
+                                    changeCount++;
+                                }
+                            } else {
+                                if(chess[k][l].equals("B")) {
+                                    changeCount++;
+                                }
+                            }
+                        }
+                    }
+                    if(changeCount < count) {
+                        count = changeCount;
+                    }
+                }
+                if(min > count) {
+                    min = count;
                 }
             }
+            }
         }
+
+        bw.write(Integer.toString(min));
+        bw.close();
     }
 }
