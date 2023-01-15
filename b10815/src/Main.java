133eadc664c151e4da181041f157e90d268cc46a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-01-15 19:14:05 +0900

    첫번째 도전

--- b10815/src/Main.java
+++ b10815/src/Main.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -25,5 +26,23 @@ public class Main {
             cards[i] = Integer.parseInt(cardsStr[i]);
         }
         
+        int[] exist = new int[m];
+        compareCards(sang, cards, n, m, exist); //정적 메소드에서는 정적 메소드만 사용 가능
+        for(int i = 0; i < m; i++){
+            bw.write(Integer.toString(exist[i]) + " ");
+        }
+        bw.close();
+    }
+
+    public static void compareCards(int[] sang, int[] cards, int n, int m, int[] exist) {
+        Arrays.fill(exist, 0); //Arrays.fill(배열이름, 초기화할 수) : 채움
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(sang[i] == cards[j]) {
+                    exist[j] = 1;
+                }
+            }
+        }
     }
 }
