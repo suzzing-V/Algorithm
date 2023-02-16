c283b52e15673ffa32ea52535674670d0513d52a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-17 00:16:48 +0900

    split으로 교체

--- b10815/src/Main.java
+++ b10815/src/Main.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.HashMap;
 
@@ -13,17 +12,19 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(bf.readLine());
-        StringTokenizer sangStr = new StringTokenizer(bf.readLine());
+        String sangStr = bf.readLine();
+        String[] sang = sangStr.split(" ");
         HashMap<Integer, Integer> sMap = new HashMap<>(n);
         for(int i = 0; i < n; i++) {
-            sMap.put(i, Integer.parseInt(sangStr.nextToken()));
+            sMap.put(i, Integer.parseInt(sang[i]));
         }
 
         int m = Integer.parseInt(bf.readLine());
-        StringTokenizer cardsStr = new StringTokenizer(bf.readLine());
+        String cardStr = bf.readLine();
+        String[] card = cardStr.split(" ");
         int[] cards = new int[m];
         for(int i = 0; i < m; i++) {
-            cards[i] = Integer.parseInt(cardsStr.nextToken());
+            cards[i] = Integer.parseInt(card[i]);
         }
         
         for(int c : cards) {
