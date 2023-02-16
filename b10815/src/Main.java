c68d9aa157861593b067ab6dc1e85ad2375f60c0
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-17 01:35:34 +0900

    이진탐색법

--- b10815/src/Main.java
+++ b10815/src/Main.java
@@ -3,8 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,26 +13,44 @@ public class Main {
         int n = Integer.parseInt(bf.readLine());
         String sangStr = bf.readLine();
         String[] sang = sangStr.split(" ");
-        HashMap<Integer, Integer> sMap = new HashMap<>(n);
+        int[] sangArr = new int[n];
         for(int i = 0; i < n; i++) {
-            sMap.put(i, Integer.parseInt(sang[i]));
+            sangArr[i] = Integer.parseInt(sang[i]);
         }
 
         int m = Integer.parseInt(bf.readLine());
         String cardStr = bf.readLine();
         String[] card = cardStr.split(" ");
-        int[] cards = new int[m];
+        int[] cardArr = new int[m];
         for(int i = 0; i < m; i++) {
-            cards[i] = Integer.parseInt(card[i]);
+            cardArr[i] = Integer.parseInt(card[i]);
         }
+        Arrays.sort(sangArr);
         
-        for(int c : cards) {
-            if(sMap.containsValue(c)) {
+        for(int c : cardArr) {
+            if(binarySearch(c, sangArr, 0, n - 1)) {
                 bw.write("1 ");
             } else {
                 bw.write("0 ");
             }
         }
         bw.close();
+    }
+
+    public static boolean binarySearch(int c, int[] sangArr, int start, int end) {
+        int mid = (start + end) / 2;
+        boolean a;
+
+        if(start > end) {
+            return false;
+        }
+        if(c > sangArr[mid]) {
+            a = binarySearch(c, sangArr, mid + 1, end);
+        } else if(c == sangArr[mid]) {
+            a = true;
+        } else {
+            a = binarySearch(c, sangArr, start, mid - 1);
+        }
+        return a;
     }
 }
