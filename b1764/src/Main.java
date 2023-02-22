e7e1879d53b448345f575be7d3f476b1056ca9e6
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-23 02:54:38 +0900

    배열 null참조

--- b1764/src/Main.java
+++ b1764/src/Main.java
@@ -13,27 +13,28 @@ public class Main {
         int n = Integer.parseInt(mn[0]);
         int m = Integer.parseInt(mn[1]);
 
-        HashMap<Integer, String> hear = new HashMap<>(n);
+        String[] hear = new String[n];
         for(int i = 0; i < n; i++) {
-            hear.put(i, bf.readLine());
+            hear[i] = bf.readLine();
         }
 
         String[] see = new String[m];
         for(int i = 0; i < m; i++) {
             see[i] = bf.readLine();
         }
+        Arrays.sort(hear);
         
         int count = 0;
         int k = 0;
         for(String name : see) {
-            if(hear.containsValue(name)) {
+            if(BSearch(hear, name, 0, n - 1)) {
                 count ++;
             }
         }
 
         String[] result = new String[count];
         for(String name : see) {
-            if(hear.containsValue(name)) {
+            if(BSearch(hear, name, 0, n - 1)) {
                 result[k] = name;
                 k++;
             }
@@ -45,5 +46,20 @@ public class Main {
             bw.write(result[i] + "\n");
         }
         bw.close();
+    }
+
+    public static boolean BSearch(String[] hear, String name, int start, int end) {
+        int mid = (start + end) / 2;
+        if(start > end) {
+            return false;
+        }
+
+        if(name.equals(hear[mid])) {
+            return true;
+        } else if(name.compareTo(hear[mid]) < 0) {
+            return BSearch(hear, name, 0, mid - 1);
+        } else {
+            return BSearch(hear, name, mid + 1, end);
+        }
     }
 }
