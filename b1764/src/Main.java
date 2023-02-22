d6a2e8b2937f0fe0b8a820c883fde55b7fb32022
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-23 02:50:29 +0900

    HashMap 이용

--- b1764/src/Main.java
+++ b1764/src/Main.java
@@ -13,23 +13,27 @@ public class Main {
         int n = Integer.parseInt(mn[0]);
         int m = Integer.parseInt(mn[1]);
 
-        String[] hear = new String[n];
+        HashMap<Integer, String> hear = new HashMap<>(n);
         for(int i = 0; i < n; i++) {
-            hear[i] = bf.readLine();
+            hear.put(i, bf.readLine());
         }
 
         String[] see = new String[m];
         for(int i = 0; i < m; i++) {
             see[i] = bf.readLine();
         }
-        Arrays.sort(hear);
         
         int count = 0;
-        String[] result = new String[m];
         int k = 0;
         for(String name : see) {
-            if(BSearch(hear, name, 0, n - 1)) {
+            if(hear.containsValue(name)) {
                 count ++;
+            }
+        }
+
+        String[] result = new String[count];
+        for(String name : see) {
+            if(hear.containsValue(name)) {
                 result[k] = name;
                 k++;
             }
@@ -41,20 +45,5 @@ public class Main {
             bw.write(result[i] + "\n");
         }
         bw.close();
-    }
-
-    public static boolean BSearch(String[] hear, String name, int start, int end) {
-        int mid = (start + end) / 2;
-        if(start > end) {
-            return false;
-        }
-
-        if(name.equals(hear[mid])) {
-            return true;
-        } else if(name.compareTo(hear[mid]) < 0) {
-            return BSearch(hear, name, 0, mid - 1);
-        } else {
-            return BSearch(hear, name, mid + 1, end);
-        }
     }
 }
