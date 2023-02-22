e1b1c4ddc626b21cfc03289f63cb372951328566
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-23 02:20:56 +0900

    1

---
+++ b1764/src/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] mn = bf.readLine().split(" ");
+        int n = Integer.parseInt(mn[0]);
+        int m = Integer.parseInt(mn[1]);
+
+        String[] hear = new String[n];
+        for(int i = 0; i < n; i++) {
+            hear[i] = bf.readLine();
+        }
+
+        String[] see = new String[m];
+        for(int i = 0; i < m; i++) {
+            see[i] = bf.readLine();
+        }
+        Arrays.sort(hear);
+
+        for(String name : see) {
+            if(BSearch(hear, name, 0, n - 1)) {
+                bw.write(name + "\n");
+            }
+        }
+        bw.close();
+    }
+
+    public static boolean BSearch(String[] hear, String name, int start, int end) {
+        int mid = (start + end) / 2;
+        if(start > end) {
+            return false;
+        }
+
+        if(name.equals(hear[mid])) {
+            return true;
+        } else if(name.compareTo(hear[mid]) < 0) {
+            return BSearch(hear, name, 0, mid - 1);
+        } else {
+            return BSearch(hear, name, mid + 1, end);
+        }
+    }
+}
