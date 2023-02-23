87dc038da657514e039f75df9819d34129c5717b
sjlim1999@naver.com <sjlim1999@naver.com>
2023-02-23 16:30:10 +0900

    해시맵

--- b11478/src/Main.java
+++ b11478/src/Main.java
@@ -3,38 +3,30 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.lang.Math;
-import java.util.*;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] str = bf.readLine().split("");
-        String[] part = new String[(int)Math.pow(2, str.length)];
+        HashMap <String, String> part = new HashMap<>();
 
-        int a = 0;
+        int count = 0;
         for(int i = 1; i <= str.length; i++) {
             for(int j = 0; j < str.length - (i - 1); j++) {
                 String element = str[j];
                 for(int k = 1; k < i; k++) {
                     element += str[j+k];
                 }
-                System.out.println(element);
-                part[a] = element;
-                a++;
+                if(!part.containsKey(element)) {
+                    part.get(element);
+                    count++;
+                }
             }
         }
 
-        String[] real = Arrays.copyOfRange(part, 0, a);
-        Arrays.sort(real);
-        int count = 1;
-        for(int i = 1; i < a; i++) {
-            if(!real[i].equals(real[i - 1])) {
-                count++;
-            }
-        }
-        System.out.println(count);
+        bw.write(String.valueOf(count));
         bw.close();
     }
 }
