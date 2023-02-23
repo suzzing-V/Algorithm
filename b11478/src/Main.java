6e0737ccbca0c7db3fd91836569c9ffadc0b2635
sjlim1999@naver.com <sjlim1999@naver.com>
2023-02-23 15:55:31 +0900

    1

--- b11478/src/Main.java
+++ b11478/src/Main.java
@@ -4,27 +4,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.Math;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String str = bf.readLine();
-        int[] count = new int[26];
-        int ex = 0;
+        String[] str = bf.readLine().split("");
+        String[] part = new String[(int)Math.pow(2, str.length)];
 
-        Arrays.fill(count, 0);
-        for(int i = 0; i < str.length(); i++) {
-            count[str.charAt(i) - 97] ++;
-        }
-
-        for(int i = 0; i < 26; i++) {
-            if(count[i] > 1) {
-                ex += count[i] - 1;
+        int a = 0;
+        for(int i = 1; i <= str.length; i++) {
+            for(int j = 0; j < str.length - (i - 1); j++) {
+                String element = str[j];
+                for(int k = 1; k < i; k++) {
+                    element += str[j+k];
+                }
+                System.out.println(element);
+                part[a] = element;
+                a++;
             }
         }
-        bw.write(String.valueOf((int)(Math.pow(2, str.length()) - 1 - ex)));
+
+        String[] real = Arrays.copyOfRange(part, 0, a);
+        Arrays.sort(real);
+        int count = 1;
+        for(int i = 1; i < a; i++) {
+            if(!real[i].equals(real[i - 1])) {
+                count++;
+            }
+        }
+        System.out.println(count);
         bw.close();
     }
 }
