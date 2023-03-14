2f58391d78057aed3b85d9ffb9f128956d112805
sjlim1999@naver.com <sjlim1999@naver.com>
2023-03-14 15:26:02 +0900

    1 틀렸습니다

---
+++ b1934/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(bf.readLine());
+        
+        for(int i = 0; i < t; i++) {
+            String[] ab = new String[2];
+            ab = bf.readLine().split(" ");
+            int a = Integer.parseInt(ab[0]), b = Integer.parseInt(ab[1]);
+            int min = a < b ? a : b;
+            int mcm = 1;
+            int d = 2;
+            while(d <= min / 2) {
+                if(a % d == 0 && b % d == 0) {
+                    mcm *= d;
+                    a /= d;
+                    b /= d;
+                } else {
+                    d++;
+                }
+            }
+            bw.write(mcm * a * b + "\n");
+        }
+        bw.close();
+    }
+}
