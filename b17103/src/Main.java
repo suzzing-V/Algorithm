171155339afdaebb616405d08fcee2819ecca17b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-18 20:16:26 +0900

    에라토스테네스의 체 이용 시간초과일 땐 배열 생각해보기 메모리 생각하지 말기

--- b17103/src/Main.java
+++ b17103/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,24 +8,27 @@ public class Main {
         int t = Integer.parseInt(bf.readLine());
         int count;
 
+        boolean[] prime = new boolean[1000001];
+        Arrays.fill(prime,true);
+
+        for(int i = 2; i < 1000001; i++) {
+            if(prime[i]) {
+                for(int j = 2; i * j < 1000001; j++) {
+                    if(prime[i * j]) { prime[i * j] = false; }
+                }
+            }
+        }
+
         for(int i = 0; i < t; i++) {
             int n = Integer.parseInt(bf.readLine());
             count = 0;
             for(int j = 2; j <= n / 2; j++) {
-                if(j % 2 != 0 && (n - j) % 2 != 0 && isPrime(j) && isPrime(n - j)) {
-                    //System.out.println("Åë°ú");
-                    count ++;
+                if(prime[j] && prime[n - j]) {
+                    count++;
                 }
             }
             bw.write(Integer.toString(count) + "\n");
         }
         bw.close();
-    }
-
-    public static boolean isPrime(int n) {
-        for(int i = 2; i <= Math.sqrt(n); i++) {
-            if(n % i == 0) { return false; }
-        }
-        return true;
     }
 }
