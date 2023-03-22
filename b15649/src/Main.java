a7253a35093fdcfe70b79e840c4a7a70b6e36211
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-22 20:47:20 +0900

    2 성공 입출력 수정

--- b15649/src/Main.java
+++ b15649/src/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] nm = new String[2];
         nm = bf.readLine().split(" ");
         int n = Integer.parseInt(nm[0]);
@@ -11,15 +12,16 @@ public class Main {
         int[] arr = new int[m];
         boolean[] visit = new boolean[n + 1];
         Arrays.fill(visit, false);
-        DFS(arr, visit, 0);
+        DFS(arr, visit, 0, bw);
+        bw.close();
     }
 
-    public static void DFS(int[] arr, boolean[] visit, int i) {
+    public static void DFS(int[] arr, boolean[] visit, int i, BufferedWriter bw) throws IOException {
         if(arr.length == i) {
             for(int j = 0; j < arr.length; j++) {
-                System.out.print(arr[j] + " ");
+                bw.write(Integer.toString(arr[j]) + " ");
             }
-            System.out.println();
+            bw.write("\n");
             return;
         }
 
@@ -27,7 +29,7 @@ public class Main {
             if(!visit[j]) {
                 arr[i] = j;
                 visit[j] = true;
-                DFS(arr, visit, i + 1);
+                DFS(arr, visit, i + 1, bw);
                 visit[j] = false;
             }
         } 
