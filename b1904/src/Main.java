2e2a82c163f86572d67745446803ec1146fdfd21
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-30 17:41:24 +0900

    3 틀렸습니다 1일 경우 예외 설정

--- b1904/src/Main.java
+++ b1904/src/Main.java
@@ -7,9 +7,11 @@ public class Main {
         int n = Integer.parseInt(bf.readLine());
         int[] tileCount = new int[n + 1];
         tileCount[1] = 1;
-        tileCount[2] = 2;
-        for(int i = 3; i <= n; i++) {
-            tileCount[i] = tileCount[i - 1] + tileCount[i - 2];
+        if(n != 1) {
+            tileCount[2] = 2;
+            for(int i = 3; i <= n; i++) {
+                tileCount[i] = tileCount[i - 1] + tileCount[i - 2];
+            }
         }
         bw.write(Integer.toString(tileCount[n]));
         bw.close();
