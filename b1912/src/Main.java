06234ce900b85e05b1be6355ef8b6f7bcffdf642
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-31 23:36:30 +0900

    시간 초과

--- b1912/src/Main.java
+++ b1912/src/Main.java
@@ -1,31 +1,37 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bf.readLine());
-        String[] arr = new String[n];
-        arr = bf.readLine().split(" ");
-        int sum = 0;
-        for(int i = 1; i <= n; i++) sum += i;
-        int[] total = new int[sum];
-        int i = 0;
-        while(i < n) {
-            total[i] = Integer.parseInt(arr[i]);
-            i++;
+        String[] arrStr = new String[n];
+        arrStr = bf.readLine().split(" ");
+
+        int[] arr = new int[n];
+        long[] total = new long[n - 1];
+        long max = -1000;
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(arrStr[i]);
+        }
+        for(int i = 0; i < n - 1; i++) {
+            total[i] = arr[i];
+            if(max <= total[i])
+                max = total[i];
         }
         
-        for(int j = 1; j <= n; j++) {
-            for(int k = j; k <= n - 1; k++) {
-                total[i] = total[k] + total[i - (n - (j - 1))];
-                i++;
+        int j;
+        for(int i = 1; i < n; i++) {
+            j = 0;
+            for(int k = i; k < n; k++) {
+                total[j] += arr[k];
+                if(max <= total[j])
+                    max = total[j];
+                j++;
             }
         }
-        Arrays.sort(total);
     
-        bw.write(Integer.toString(total[i - 1]));
+        bw.write(Long.toString(max));
         bw.close();
     }
 }
