697f6f7d0eb86590e6fde1a4648816f59e252966
sjlim1999@naver.com <sjlim1999@naver.com>
2023-04-05 20:42:50 +0900

    2 틀렸습니다

--- b10844/src/Main.java
+++ b10844/src/Main.java
@@ -5,41 +5,24 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(bf.readLine());
-
-        int[] before = new int[10];
-        int[] after = new int[10];
-        int[] d = new int[N + 1];
-        before[0] = 0;
-        for(int i = 1; i <= 9; i++) {
-            before[i] = 1;
-        }
+        long[] d = new long[N + 1];
 
         d[1] = 9;
         for(int i = 2; i <= N; i++) {
-            fillCountArr(before, after);
-            d[i] = sumArrDiv(after);
-            before = after;
-            after = new int[10];
+            d[i] = d[i - 1] * 2 - (i * 2 - 3);
+            d[i] %= 1000000000;
         }
         
-        bw.write(Integer.toString(d[N]));
+        bw.write(Long.toString(d[N]));
         bw.close();
     }
 
-    public static void fillCountArr(int[] before, int[] after) {
+    public static void fillCountArr(long[] before, long[] after) {
         for(int i = 2; i <= 8; i++) {
             after[i] = before[i - 1] + before[i + 1];
         }
         after[0] = before[1];
         after[1] = before[2];
         after[9] = before[8];
-    }
-
-    public static int sumArrDiv(int[] after) {
-        int sum = 0;
-        for(int i = 0; i <= 9; i++) {
-            sum += after[i] % 1000000000;
-        }
-        return sum;
     }
 }
