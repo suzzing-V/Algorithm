5be4d3559103db5a1ac2c78816e899cf4983cb74
sjlim1999@naver.com <sjlim1999@naver.com>
2023-04-10 17:49:52 +0900

    3 런타임 에러

--- b9252/src/Main.java
+++ b9252/src/Main.java
@@ -1,39 +1,25 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] arr1 = bf.readLine().toCharArray();
-        char[] arr2 = bf.readLine().toCharArray();
-        int max = 0;
-        boolean[] count = new boolean[91];
-        int[] memo = new int[arr1.length];
-        int p = 0;
-        int fp = 0;;
+        char[] arr1 = (" " + bf.readLine()).toCharArray();
+        char[] arr2 = (" " + bf.readLine()).toCharArray();
+        int[][] memo = new int[arr1.length + 1][arr2.length + 1];
 
-        for(int i = 0; i < arr1.length; i++) {
-            fp = p;
-            while(p < arr2.length) {
-                if(arr1[i] == arr2[p]) {
-                    max = 0;
-                    for(int j = 0; j < i; j++) {
-                        if(count[arr1[j]] == true
-                            && max < memo[j]) {
-                            max = memo[j];
-                        }
-                    }
-                    memo[i] = max + 1;
-                    count[arr1[i]] = true;
-                    break;
+
+        for(int i = 1; i < arr1.length; i++) {
+            for(int j = 1; j < arr2.length; j++) {
+                if(arr1[i] == arr2[j]) {
+                    memo[j][i] = memo[j - 1][i - 1] + 1;
+                } else {
+                    memo[j][i] = Math.max(memo[j - 1][i], memo[j][i - 1]);
                 }
-                p++;
             }
-            if(p == arr2.length) p = fp;
         }
-        Arrays.sort(memo);
-        bw.write(Integer.toString(memo[arr1.length - 1]));
+        
+        bw.write(Integer.toString(memo[arr2.length - 1][arr1.length - 1]));
         bw.close();
     }
 }
