c9247a1abd867d4360ce4726f1780d478c9a2aa6
sjlim1999@naver.com <sjlim1999@naver.com>
2023-04-10 17:59:20 +0900

    4 성공 찾아봄

--- b9252/src/Main.java
+++ b9252/src/Main.java
@@ -4,22 +4,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] arr1 = (" " + bf.readLine()).toCharArray();
-        char[] arr2 = (" " + bf.readLine()).toCharArray();
+        char[] arr1 = (bf.readLine()).toCharArray();
+        char[] arr2 = (bf.readLine()).toCharArray();
         int[][] memo = new int[arr1.length + 1][arr2.length + 1];
 
-
-        for(int i = 1; i < arr1.length; i++) {
-            for(int j = 1; j < arr2.length; j++) {
-                if(arr1[i] == arr2[j]) {
-                    memo[j][i] = memo[j - 1][i - 1] + 1;
+        for(int i = 1; i <= arr1.length; i++) {
+            for(int j = 1; j <= arr2.length; j++) {
+                if(arr1[i - 1] == arr2[j - 1]) {
+                    memo[i][j] = memo[i - 1][j - 1] + 1;
                 } else {
-                    memo[j][i] = Math.max(memo[j - 1][i], memo[j][i - 1]);
+                    memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
                 }
             }
         }
         
-        bw.write(Integer.toString(memo[arr2.length - 1][arr1.length - 1]));
+        bw.write(Integer.toString(memo[arr1.length][arr2.length]));
         bw.close();
     }
 }
