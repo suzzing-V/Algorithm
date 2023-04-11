6a9f477bd302a37cc263019cf744be006a65252a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-11 16:56:01 +0900

    1 성공

--- b27866/src/Main.java
+++ b27866/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str = bf.readLine();
         int i = Integer.parseInt(bf.readLine());
-        bw.write(str.charAt(i - 1));
+        bw.write(str.charAt(i - 1)); //char도 그냥 출력됨
         bw.close();
     }
 }
