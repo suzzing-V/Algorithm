f7cbb49a9440e39d251c786e034f0a92427c858b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-11 18:02:29 +0900

    null이면 길이 잴 수 없다

--- b11718/src/Main.java
+++ b11718/src/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 0; i < 100; i++) {
-            String str = bf.readLine();
-            if(str.length() == 0) break;
+        String str;
+        while(true) {
+            str = bf.readLine();
+            if(str == null) break;
             bw.write(str + "\n");
         }
         bw.close();
