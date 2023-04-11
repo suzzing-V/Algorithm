8d0afe2297f6766c8a08eed5205e8a6c11477e2d
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-11 17:34:00 +0900

    2 런타임 에러

--- b11718/src/Main.java
+++ b11718/src/Main.java
@@ -1,16 +1,14 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        Scanner sc = new Scanner(System.in);
-        String str;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for(int i = 0; i < 100; i++) {
-            str = sc.nextLine();
-            if(str.length() == 0)
-                break;
-            System.out.println(str);
+            String str = bf.readLine();
+            if(str.length() == 0) break;
+            bw.write(str + "\n");
         }
-        sc.close();
+        bw.close();
     }
 }
