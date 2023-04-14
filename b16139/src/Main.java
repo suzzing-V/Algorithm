e57d278f278b92bd7ee92bfbdf41faa7ee450252
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-14 11:45:20 +0900

    4 성공 찾아봄 2차원배열 생성

--- b16139/src/Main.java
+++ b16139/src/Main.java
@@ -3,39 +3,39 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String s = bf.readLine();
 		long q = Long.parseLong(bf.readLine());
+		
+		long[][] acc = new long[26][s.length()];
+		acc[s.charAt(0) - 'a'][0] = 1;
+		for(int i = 1; i < s.length(); i++) {
+			for(int j = 0; j < 26; j++) {
+				if(s.charAt(i) == j + 'a') {
+					acc[j][i] = acc[j][i - 1] + 1;
+				} else {
+					acc[j][i] = acc[j][i - 1];
+				}
+			}
+		}
 		
 		char a;
 		int l, r;
 		long result;
-		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 1; i <= q; i++) {
 			String[] line = new String[3];
 			line = bf.readLine().split(" ");
 			a = line[0].charAt(0);
 			l = Integer.parseInt(line[1]);
 			r = Integer.parseInt(line[2]);
-			
-			long[] acc = new long[s.length()];
-			getAccumulateArr(s, a, acc);
+
 			if(l == 0) {
-				result = acc[r];
+				result = acc[a - 'a'][r];
 			} else {
-				result = acc[r] - acc[l - 1];
+				result = acc[a - 'a'][r] - acc[a - 'a'][l - 1];
 			}
-			bw.write(Long.toString(result) + "\n");
+			sb.append(result + "\n");
 		}
-		bw.close();
-		
-	}
-	
-	public static void getAccumulateArr(String s, char a, long[] acc) {
-		if(s.charAt(0) == a) { acc[0] = 1; }
-		for(int i = 1; i < s.length(); i++) {
-			if(s.charAt(i) == a) { acc[i] = acc[i - 1] + 1; }
-			else { acc[i] = acc[i - 1]; }
-		}
+		System.out.println(sb);
 	}
 }
