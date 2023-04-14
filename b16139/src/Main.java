0b9557efded00b978d82979c814f96191128dfa9
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-14 11:03:00 +0900

    1 50점

--- b16139/src/Main.java
+++ b16139/src/Main.java
@@ -1,5 +1,40 @@
+import java.io.*;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("hi");
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String s = bf.readLine();
+		int q = Integer.parseInt(bf.readLine());
+		
+		char a;
+		int l, r, result;
+		
+		for(int i = 1; i <= q; i++) {
+			String[] line = new String[3];
+			line = bf.readLine().split(" ");
+			a = line[0].charAt(0);
+			l = Integer.parseInt(line[1]);
+			r = Integer.parseInt(line[2]);
+			
+			int[] acc = new int[s.length()];
+			getAccumulateArr(s, a, acc);
+			if(l == 0) {
+				result = acc[r];
+			} else {
+				result = acc[r] - acc[l - 1];
+			}
+			bw.write(Integer.toString(result) + "\n");
+		}
+		bw.close();
+		
+	}
+	
+	public static void getAccumulateArr(String s, char a, int[] acc) {
+		if(s.charAt(0) == a) { acc[0] = 1; }
+		for(int i = 1; i < s.length(); i++) {
+			if(s.charAt(i) == a) { acc[i] = acc[i - 1] + 1; }
+			else { acc[i] = acc[i - 1]; }
+		}
 	}
 }
