097bb555ad51f272b884a1bdbe11ea1d3714df68
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-22 17:52:17 +0900

    2 시간초과 나머지 0일 경우

--- b1629/src/Main.java
+++ b1629/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 
 public class Main {
+	//int[] remain = new int[2147483647];
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -17,9 +18,11 @@ public class Main {
 	}
 	
 	public static int divideAndRemain(int a, int b, int c) {
-		if(b == 1) { return a % c; }
-		
-		int result = (divideAndRemain(a, b / 2, c) * divideAndRemain(a, b - b / 2, c)) % c;
+		if(b == 1 || a == c) { return a % c; }
+		int r1 = divideAndRemain(a, b / 2, c);
+		int r2 = divideAndRemain(a, b - b / 2, c);
+		if(r1 == 0 || r2 == 0) { return 0; }
+		int result = (r1 * r2) % c;
 		return result;
 	}
 }
