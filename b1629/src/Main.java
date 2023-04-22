7269fa05229e4344b44ace6cd408923ce133a7a5
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-22 19:45:08 +0900

    4 성공 홀수 짝수 나눠서 한번만 계산하도록

--- b1629/src/Main.java
+++ b1629/src/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 
 public class Main {
-	static int[] remain = new int[1000000];
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,20 +11,15 @@ public class Main {
 		int b = Integer.parseInt(line[1]);
 		int c = Integer.parseInt(line[2]);
 		
-		int result = divideAndRemain(a, b, c);
-		bw.write(Integer.toString(result));
+		long result = divideAndRemain(a, b, c);
+		bw.write(Long.toString(result));
 		bw.close();
 	}
 	
-	public static int divideAndRemain(int a, int b, int c) {
-		if(b == 1 || a % c == 0) { return a % c; }
-		if(b < 1000000 && remain[b] != 0) { return remain[b]; }
-		int r1 = divideAndRemain(a, b / 2, c);
-		int r2 = divideAndRemain(a, b - b / 2, c);
-		if(r1 == 0 || r2 == 0) { return 0; }
-		int result = (r1 * r2) % c;
-		if(b < 1000000)
-			remain[b] = result;
-		return result;
+	public static long divideAndRemain(int a, int b, int c) {
+		if(b == 1) { return a % c; }
+		long result = divideAndRemain(a, b / 2, c) % c;
+		if(b % 2 == 0) { return result* result % c; }
+		else { return ((result * result % c)  * a % c); }
 	}
 }
