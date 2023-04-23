fadd8d1f58b548821cf76816ebc6312e6034fea2
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-23 14:05:24 +0900

    2 찾아봄 팩토리얼, 페르마의 소정리 이용

--- b11401/src/Main.java
+++ b11401/src/Main.java
@@ -9,17 +9,32 @@ public class Main {
 		line = bf.readLine().split(" ");
 		int n = Integer.parseInt(line[0]);
 		int k = Integer.parseInt(line[1]);
-		long result = ((getRemain(n, k) % mod) / (getRemain(k, k) % mod)) % mod;
+		
+		long nF = factorial(n);
+		long nkF = factorial(n - k);
+		long kF = factorial(k);
+		long result = (nF * powRemain((nkF * kF) % mod, mod - 2)) % mod;
 		bw.write(Long.toString(result));
 		bw.close();
 	}
 	
-	public static long getRemain(int n, int k) {
-		if(k == 0) {
+	public static long factorial(int n) {
+		if(n == 1 || n == 0) {
 			return 1;
 		}
+		return ((n % mod) * (factorial(n - 1) % mod)) % mod;  
+	}
+	
+	public static long powRemain(long base, long expo) {
+		if(expo == 1) {
+			return base % mod;
+		}
 		
-		long r = n % mod;
-		return r * getRemain(n - 1, k - 1);
+		long tmp = powRemain(base, expo / 2);
+		if(expo % 2 == 0) {
+			return (tmp * tmp) % mod;
+		} else {
+			return (((tmp * tmp) % mod) * (base % mod)) % mod;
+		}
 	}
 }
