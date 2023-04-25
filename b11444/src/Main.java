2ef3be76de9463337c0cd1fe61a019863211b464
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-25 23:54:23 +0900

    2 찾아봄 1번째 피보나치는 1

--- b11444/src/Main.java
+++ b11444/src/Main.java
@@ -1,30 +1,26 @@
 import java.io.*;
 
 public class Main {
-	static int mod = 1000000007;
+	static long mod = 1000000007;
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		long n = Long.parseLong(bf.readLine());
 		long[][] arr = new long[2][2];
-		if(n == 1) {
-			bw.write(Integer.toString(0));
-		} else {
-			arr[0][0] = 1; arr[0][1] = 1;
-			arr[1][0] = 1; arr[1][1] = 0;
-			bw.write(Long.toString((pow(arr, n - 1))[0][0]));
-		}
+		arr[0][0] = 1; arr[0][1] = 1;
+		arr[1][0] = 1; arr[1][1] = 0;
+		bw.write(Long.toString((pow(arr, n - 1))[0][0]));
 		bw.close();
 	}
 	
 	public static long[][] pow(long[][] arr, long expo) {
-		if(expo == 1) {
+		if(expo == 1 || expo == 0) {
 			return arr;
 		}
 		
 		long[][] tmp = pow(arr, expo / 2);
 		if(expo % 2 == 0) { return multiply(tmp, tmp); }
-		return multiply(multiply(tmp, tmp),arr);
+		return multiply(multiply(tmp, tmp), arr);
 	}
 	
 	public static long[][] multiply(long[][] arr1, long[][] arr2) {
