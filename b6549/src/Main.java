c907f16fcfe5df9ac7bb4c8e1499254aaade312b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-04-27 15:46:46 +0900

    2 성공 찾아봄

--- b6549/src/Main.java
+++ b6549/src/Main.java
@@ -12,44 +12,62 @@ public class Main {
 			line = bf.readLine().split(" ");
 	
 			int n = Integer.parseInt(line[0]);
-			long[] arr = new long[n];
+			long[] hist = new long[n];
 			if(n == 0) break;
 			for(int i = 0; i < n; i++) {
-				arr[i] = Long.parseLong(line[i + 1]);
+				hist[i] = Long.parseLong(line[i + 1]);
 			}
 			
-			for(int i = 0; i < n; i++) {
-				left = maxRight(arr[i], arr, i);
-				right = maxLeft(arr[i], arr, i);
-				
-				r = (left + right + 1) * arr[i];
-				if(r > max) max = r;
-			}
-			bw.write(Long.toString(max) + "\n");
+			bw.write(Long.toString(maxRectangle(hist, 0, n - 1)) + "\n");
 		}
 		
 		bw.close();
 	}
 	
-	public static long maxRight(long h, long[] arr, int c) {
-		long length = 0;
-		for(int i = c + 1; i < arr.length; i++) {
-			if(arr[i] < h) break;
-			else {
-				length++;
-			}
-		}
-		return length;
+	public static long maxRectangle(long[] hist, int start, int end) {
+		if(start == end)
+			return hist[start];
+		
+		int mid = (start + end) / 2;
+		long left = maxRectangle(hist, start, mid);
+		long right = maxRectangle(hist, mid + 1, end);
+		long max = Math.max(left, right);
+		
+		long midArea = getMidArea(hist, start, end);
+		max = Math.max(max, midArea);
+		return max;
 	}
 	
-	public static long maxLeft(long h, long[] arr, int c) {
-		long length = 0;
-		for(int i = c - 1; i >= 0; i--) {
-			if(arr[i] < h) break;
-			else {
-				length++;
+	public static long getMidArea(long[] hist, int start, int end) {
+		int left = (start + end) / 2;
+		int right = (start + end) / 2;
+		long tmp = 0;
+		long minHeight = hist[left];
+		long maxArea = 0;
+		while(left > start && right < end) {
+			if(hist[left - 1] > hist[right + 1]) {
+				left--;
+				tmp = hist[left];
+			} else {
+				right ++;
+				tmp = hist[right];
 			}
+			minHeight = Math.min(minHeight, tmp);
+			maxArea = Math.max(minHeight * (right - left + 1), maxArea);
 		}
-		return length;
+		
+		while(left > start) {
+			left--;
+			minHeight = Math.min(minHeight, hist[left]);
+			maxArea = Math.max(minHeight * (right - left + 1), maxArea);
+		}
+		
+		while(right < end) {
+			right++;
+			minHeight = Math.min(minHeight, hist[right]);
+			maxArea = Math.max(minHeight * (right - left + 1), maxArea);
+		}
+
+		return maxArea;
 	}
 }
