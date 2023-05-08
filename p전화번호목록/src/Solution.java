17dd1ce1ed63211431be9ebd9747c42b53c2edfc
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-08 23:58:51 +0900

    3 성공 비교연산자는 시간 오래 걸림

--- p전화번호목록/src/Solution.java
+++ p전화번호목록/src/Solution.java
@@ -4,16 +4,13 @@ class Solution {
     public boolean solution(String[] phone_book) {
         HashMap<String, Integer> hm = new HashMap<>();
         for(String number: phone_book) {
-            hm.put(number, number.length());
+            hm.put(number, 0);
         }
         
         for(int i = 0; i < phone_book.length; i++) {
-            int length = hm.get(phone_book[i]);
-            for(String number : phone_book) {
-                if(number.length() > length) {
-                    String cut = number.substring(0, length);
+            for(int j = 1; j < phone_book[i].length(); j++) {
+                    String cut = phone_book[i].substring(0, j);
                     if(hm.containsKey(cut)) return false;
-                }
             }
         }
         return true;
