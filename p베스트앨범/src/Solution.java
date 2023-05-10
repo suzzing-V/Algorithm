7690f61b9ea669104ae3785c2021db232a49710b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-11 02:15:49 +0900

    3 객체의 요소를 기준으로 내림차순으로 정렬하는 방법

--- p베스트앨범/src/Solution.java
+++ p베스트앨범/src/Solution.java
@@ -1,69 +1,64 @@
 import java.util.*;
 
 class Song {
-    public int index;
-    public String genre;
+    int index;
+    String genre;
+    int play;
     
-    public Song(int index, String genre) {
+    public Song(int index, String genre, int play) {
         this.index = index;
         this.genre = genre;
+        this.play = play;
     }
 }
 
 class Solution {
-	public static void main(String[] args) {
-		String[] genres = { "classic", "pop", "classic", "classic", "pop" };
-		int[] plays = { 500, 600, 150, 800, 2500 };
-		int[] res = solution(genres, plays);
-	}
-	
-    public static int[] solution(String[] genres, int[] plays) {
-    	 List<Integer> answer = new ArrayList<>();
-         HashMap<Integer, Song> hm = new HashMap<>();
-         for(int i = 0; i < plays.length; i++) {
-             hm.put(plays[i], new Song(i, genres[i]));
-         }
-         
-         HashMap<String, Integer> total = new HashMap<>();
-         for(int i = 0; i < plays.length; i++) {
-             total.put(genres[i], total.getOrDefault(genres[i], 0) + plays[i]);
-         }
-         List<String> geOrder = new ArrayList<>();
-         Set<String> keySet = total.keySet();
-         while(keySet.size() != 0) {
-             int max = 0;
-             String maxGe = "";
-             Iterator<String> iterator = keySet.iterator();
-             while(iterator.hasNext()) {
-                 String genre = iterator.next();
-                 if(total.get(genre) > max) {
-                     max = total.get(genre);
-                     maxGe = genre;
-                 }
-             }
-             geOrder.add(maxGe);
-             keySet.remove(maxGe);
+    public int[] solution(String[] genres, int[] plays) {
+        List<Song> answer = new ArrayList<>();
+        
+        HashMap<String, Integer> total = new HashMap<>();
+        for(int i = 0; i < plays.length; i++) {
+            total.put(genres[i], total.getOrDefault(genres[i], 0) + plays[i]);
+        }
+        List<String> geOrder = new ArrayList<>();
+        Set<String> keySet = total.keySet();
+        while(keySet.size() != 0) {
+            int max = 0;
+            String maxGe = "";
+            Iterator<String> iterator = keySet.iterator();
+            while(iterator.hasNext()) {
+                String key = iterator.next();
+                if(total.get(key) > max) {
+                    max = total.get(key);
+                    maxGe = key;
+                }
             }
-         Arrays.sort(plays);
-         Iterator<String> iterator2 = geOrder.iterator();
-         while(iterator2.hasNext()) {
-             String genre = iterator2.next();
-             List<Integer> tmp = new ArrayList<>();
-             for(int i = plays.length - 1; i >= 0; i--) {
-                 if(genre.equals(hm.get(plays[i]).genre)) {
-                     tmp.add(hm.get(plays[i]).index);
-                 }
-                 if(tmp.size() == 2) break;
-             }
-         
-             for(int i : tmp) {
-                 answer.add(i);
-             }
-         }
-         
-         int[] result = answer.stream()
-             .mapToInt(Integer :: intValue)
-             .toArray();
-         return result;
+            geOrder.add(maxGe);
+            keySet.remove(maxGe);
+        }
+
+        Iterator<String> iterator2 = geOrder.iterator();
+        while(iterator2.hasNext()) {
+            String ge = iterator2.next();
+            List<Song> tmp = new ArrayList<>();
+            for(int i = 0; i < genres.length; i ++) {
+                if(ge.equals(genres[i])) {
+                    tmp.add(new Song(i, genres[i], plays[i]));
+                }
+            }
+        
+            Collections.sort(tmp, (o1, o2) -> o2.play - o1.play);
+            answer.add(tmp.get(0));
+            if(tmp.size() > 1) {
+                answer.add(tmp.get(1));
+            }
+        }
+        
+        int[] result = new int[answer.size()];
+        for(int i=0; i<answer.size(); i++){
+            result[i] = answer.get(i).index;
+        }
+        
+        return result;
     }
 }
