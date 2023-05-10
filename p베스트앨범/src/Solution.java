09bea1b1cccb1111a4593c77c77b5f25b821aa9a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-11 01:39:19 +0900

    1 테케 두개 실패

--- p베스트앨범/src/Solution.java
+++ p베스트앨범/src/Solution.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Song {
+class Song {
     public int index;
     public String genre;
     
@@ -11,35 +11,59 @@ public class Song {
 }
 
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
-        
-        HashMap<Integer, Song> hm = new HashMap<>();
-        
-        for(int i = 0; i < plays.length; i++) {
-            hm.put(plays[i], new Song(i, genres[i]));
-        }
-        
-        Arrays.sort(plays);
-        
-        int k = 0;
-        for(int i = plays.length - 1; i >= 0; i--) {
-            if(plays[i] != 0) {
-                //String ge = hm.get(plays[i]).genre;
-                
-                for(int j = i - 1; j >= 0; j--) {
-                    if(hm.get(plays[j]).genre.equals(hm.get(plays[i]).genre)) {
-                        answer.add(hm.get(plays[j]).index);
-                        plays[j] = 0;
-                        break;
-                    }
-                }
+	public static void main(String[] args) {
+		String[] genres = { "classic", "pop", "classic", "classic", "pop" };
+		int[] plays = { 500, 600, 150, 800, 2500 };
+		int[] res = solution(genres, plays);
+	}
+	
+    public static int[] solution(String[] genres, int[] plays) {
+    	 List<Integer> answer = new ArrayList<>();
+         HashMap<Integer, Song> hm = new HashMap<>();
+         for(int i = 0; i < plays.length; i++) {
+             hm.put(plays[i], new Song(i, genres[i]));
+         }
+         
+         HashMap<String, Integer> total = new HashMap<>();
+         for(int i = 0; i < plays.length; i++) {
+             total.put(genres[i], total.getOrDefault(genres[i], 0) + plays[i]);
+         }
+         List<String> geOrder = new ArrayList<>();
+         Set<String> keySet = total.keySet();
+         while(keySet.size() != 0) {
+             int max = 0;
+             String maxGe = "";
+             Iterator<String> iterator = keySet.iterator();
+             while(iterator.hasNext()) {
+                 String genre = iterator.next();
+                 if(total.get(genre) > max) {
+                     max = total.get(genre);
+                     maxGe = genre;
+                 }
+             }
+             geOrder.add(maxGe);
+             keySet.remove(maxGe);
             }
-        }
-        
-        int[] result = answer.stream()
-            .mapToInt(Integer :: intValue)
-            .toArray();
-        return result;
+         Arrays.sort(plays);
+         Iterator<String> iterator2 = geOrder.iterator();
+         while(iterator2.hasNext()) {
+             String genre = iterator2.next();
+             List<Integer> tmp = new ArrayList<>();
+             for(int i = plays.length - 1; i >= 0; i--) {
+                 if(genre.equals(hm.get(plays[i]).genre)) {
+                     tmp.add(hm.get(plays[i]).index);
+                 }
+                 if(tmp.size() == 2) break;
+             }
+         
+             for(int i : tmp) {
+                 answer.add(i);
+             }
+         }
+         
+         int[] result = answer.stream()
+             .mapToInt(Integer :: intValue)
+             .toArray();
+         return result;
     }
 }
