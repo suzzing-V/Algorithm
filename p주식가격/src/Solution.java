5f48583b76009e6da3a37ce49fa093cb1dee1662
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-15 16:14:29 +0900

    4 성공 스택 이용

--- p주식가격/src/Solution.java
+++ p주식가격/src/Solution.java
@@ -3,14 +3,22 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        int count = 0;
-        for(int i = 0; i < prices.length; i++) {
-            count = 0;
-            for(int j = i + 1; j < prices.length; j++) {
-                count++;
-                if(prices[j] < prices[i]) break;
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(0);
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] < prices[i - 1]) {
+                while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                    answer[stack.peek()] = i - stack.peek();
+                    stack.pop();
+                }
             }
-            answer[i] = count;
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - 1 - stack.peek();
+            stack.pop();
         }
         return answer;
     }
