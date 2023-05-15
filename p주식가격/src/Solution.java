73012617489fa403ee9dfac5355749d3db296925
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-15 15:37:38 +0900

    4 성공 스택 이용 안함

--- p주식가격/src/Solution.java
+++ p주식가격/src/Solution.java
@@ -3,30 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        int count = 0;
         for(int i = 0; i < prices.length; i++) {
-            stack.push(prices[i]);
-        }
-        
-        int j = prices.length - 1;
-        while(!stack.isEmpty()) {
-            int pop = stack.pop();
-            
-            int i = pop - 1;
-            while(i > 0) {
-                if(hm.containsKey(i)) {
-                    answer[j] = hm.get(i) - j;
-                    break;
-                }
-                i--;
+            count = 0;
+            for(int j = i + 1; j < prices.length; j++) {
+                count++;
+                if(prices[j] < prices[i]) break;
             }
-            if(i == 0) {
-                answer[j] = prices.length - 1 - j;
-            }
-            
-            if(!hm.containsKey(pop)) hm.put(pop, j);
-            j--;
+            answer[i] = count;
         }
         return answer;
     }
