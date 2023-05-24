788a8ff12532daa2a2f9e70c3cce834e00752cbe
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-25 03:54:35 +0900

    3 clothes에 이상한 값 넣음

--- p체육복/src/Solution.java
+++ p체육복/src/Solution.java
@@ -3,29 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        boolean[] count = new boolean[31];
-        int both = 0; //여벌옷 있는데 도난당한 경우
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < lost.length; i++) {
-            count[lost[i]] = true;
-        }
-        for(int i = 0; i < reserve.length; i++) {
-            if(count[reserve[i]]) both++;
-            set.add(reserve[i]);
-        }
-        
-        int borrow = 0;
-        Arrays.sort(lost);
-        for(int i = 0; i < lost.length; i++) {
-            if(set.contains(lost[i] - 1)) { //1 작은 옷 있는 경우
-                set.remove(lost[i] - 1);
-                borrow++;
-            } else if(set.contains(lost[i] + 1)) { //1 큰 옷 있는 경우
-                set.remove(lost[i] + 1);
-                borrow++;
+        int[] clothes = new int[n + 1];
+        Arrays.fill(clothes, 1);
+        for(int i = 0; i < lost.length; i++) clothes[lost[i]]--;
+        for(int i = 0; i < reserve.length; i++) clothes[reserve[i]]++;
+        for(int i = 1; i <= n; i++) {
+            if(clothes[i] == 0) {
+                if(clothes[i - 1] == 2) {
+                    clothes[i]++;
+                    clothes[i - 1] --;
+                } else if(i != n && clothes[i + 1] == 2) {
+                    clothes[i]++;
+                    clothes[i + 1]--;
+                }
             }
         }
-        answer = n - (lost.length - both) + borrow + both;
+        
+        for(int i = 1; i <= n; i++) {
+            if(clothes[i] >= 1) answer++;
+        }
         return answer;
     }
 }
