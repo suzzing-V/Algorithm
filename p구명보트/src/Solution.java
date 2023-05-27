4dc6d8082f398fb8761ea7006b8d8e7d6ea40b16
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-28 01:03:24 +0900

    3 성공 문제 제대로 읽기

--- p구명보트/src/Solution.java
+++ p구명보트/src/Solution.java
@@ -1,38 +1,24 @@
 import java.util.*;
+import java.util.Collections;
 
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        List<Integer> reverse = new ArrayList<>();
-        for(int i = 0; i < people.length; i++) {
-            reverse.add(people[people.length - 1 - i]);
-        }
-
-        while(!reverse.isEmpty()) {
-            int tmp = reverse.get(0);
-            reverse.remove(0);
-            getBoat(limit - tmp, reverse);
+        
+        int left = 0;
+        int right = people.length - 1;
+        while(left < right) {
+            if(people[left] + people[right] <= limit) {
+                right --;
+                left ++;
+            } else {
+                right --;
+            }
             answer++;
         }
+        
+        if(left == right) answer ++;
         return answer;
-    }
-    
-    public void getBoat(int rest, List<Integer> reverse) {
-        if(rest == 0) return ;
-        int kg = 0;
-        int index = 0;
-        for(int i = 0; i < reverse.size(); i++) {
-            if(reverse.get(i) <= rest) {
-                kg = reverse.get(i);
-                index = i;
-                break;
-            }
-        }
-        if(kg == 0) return;
-        else {
-            reverse.remove(index);
-            getBoat(rest - kg, reverse);
-        }
     }
 }
