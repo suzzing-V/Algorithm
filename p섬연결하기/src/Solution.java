857d913911743f7e4bcbe105d197a4fbf33d5625
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-29 16:00:35 +0900

    4 성공 뿌리 바꿀 때 그 섬의 뿌리랑 비교해야 하는데 그 섬의 번호랑 비교하던 걸 수정

--- p섬연결하기/src/Solution.java
+++ p섬연결하기/src/Solution.java
@@ -13,21 +13,20 @@ class Solution {
         for(int i = 0; i < root.length; i++) { //처음에 자신의 뿌리는 자신이다
             root[i] = i;
         }
-        
-        int count = 0;
-        for(int i = 0; count < n - 1 || i < costs.length; i++) { //n - 1까지 중복 없이 연결 가능
+
+        for(int i = 0; i < costs.length; i++) { //n - 1까지 중복 없이 연결 가능
             if(root[costs[i][0]] != root[costs[i][1]]) { //뿌리가 다르면 연결
                 connectIsland(root, costs[i][0], costs[i][1]);
                 answer += costs[i][2];
-                count++;
             }
         }
         return answer;
     }
     
     public void connectIsland(int[] root, int root1, int root2) {
+        int change = root[root2];
         for(int i = 0; i < root.length; i++) {
-            if(root[i] == root2) { //뿌리 root2인 요소들 다 뿌리 root1이도록 만들기
+            if(root[i] == change) { //뿌리 root2인 요소들 다 뿌리 root1이도록 만들기
                 root[i] = root[root1];
             }
         }
