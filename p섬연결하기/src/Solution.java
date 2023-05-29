d5ecb399d7e33121596f50d63ace303bd110df8d
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-29 16:16:56 +0900

    효율성 올리고 섬 번호가 오름차순이지 않을 경우 고려

--- p섬연결하기/src/Solution.java
+++ p섬연결하기/src/Solution.java
@@ -14,20 +14,23 @@ class Solution {
             root[i] = i;
         }
 
-        for(int i = 0; i < costs.length; i++) { //n - 1까지 중복 없이 연결 가능
+        int count = 0;
+        for(int i = 0; count < n - 1 || i < costs.length; i++) { //n - 1까지 중복 없이 연결 가능
             if(root[costs[i][0]] != root[costs[i][1]]) { //뿌리가 다르면 연결
                 connectIsland(root, costs[i][0], costs[i][1]);
                 answer += costs[i][2];
+                count ++;
             }
         }
         return answer;
     }
     
     public void connectIsland(int[] root, int root1, int root2) {
-        int change = root[root2];
+        int change = Math.max(root[root1], root[root2]); //뿌리가 더 큰 요소의 뿌리
+        int fix = Math.min(root[root1], root[root2]);
         for(int i = 0; i < root.length; i++) {
-            if(root[i] == change) { //뿌리 root2인 요소들 다 뿌리 root1이도록 만들기
-                root[i] = root[root1];
+            if(root[i] == change) { //뿌리가 더 큰 요소의 뿌리를 작은 요소의 뿌리로 바꿔야함
+                root[i] = fix;
             }
         }
     }
