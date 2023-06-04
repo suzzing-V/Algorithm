88cac6349040f6a37b52416226056e6ec9e76bd8
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-06-04 13:40:54 +0900

    6 시간초과 입력방식 수정

--- b2933/src/Main.java
+++ b2933/src/Main.java
@@ -27,86 +27,84 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String[] rc = new String[2];
-		rc = bf.readLine().split(" ");
-		int r = Integer.parseInt(rc[0]);
-		int c = Integer.parseInt(rc[1]);
+		StringTokenizer st = new StringTokenizer(bf.readLine());
 		
-		String[][] field = new String[r][c];
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		char[][] field = new char[r][c];
+		
 		for(int i = 0; i < r; i++) {
-			String[] line = new String[c];
-			line = bf.readLine().split("");
+			char[] line = bf.readLine().toCharArray();
 			for(int j = 0; j < c; j++) {
 				field[i][j] = line[j];
 			}
 		}
 		
 		int n = Integer.parseInt(bf.readLine());
-		String[] height = new String[n];
-		height = bf.readLine().split(" ");
+		int[] height = new int[n];
+		st = new StringTokenizer(bf.readLine());
+		for(int i = 0; i < n; i++) {
+			height[i] = Integer.parseInt(st.nextToken());
+		}
 		
 		for(int i = 0; i < n; i++) {
-			throwStick(field, r - Integer.parseInt(height[i]), i + 1);
+			throwStick(field, r - height[i], i + 1);
 			//printField(field);
 		}
 		
 		printField(field);
 	}
 	
-	public static void throwStick(String[][] field, int x, int order) {
+	public static void throwStick(char[][] field, int x, int order) {
 		int i = 0;
 		if(order % 2 == 1) {
-			while(i < field[0].length && field[x][i].equals(".")) i++;
+			while(i < field[0].length && field[x][i] == '.') i++;
 			if(i == field[0].length) return;
-			field[x][i] = ".";
+			field[x][i] = '.';
 		} else {
 			i = field[0].length - 1;
-			while(i >= 0 && field[x][i].equals(".")) i--;
+			while(i >= 0 && field[x][i] == '.') i--;
 			if(i < 0) return;
-			field[x][i] = ".";
+			field[x][i] = '.';
 		}
 		searchMineral(field, x, i);
 	}
 
-	public static void searchMineral(String[][] field, int x, int y) {
-		if(x - 1 >= 0 && field[x - 1][y].equals("x")) {
-			Set<Pos> cluster = new HashSet<>();
-			Set<Pos> set = new HashSet<>();
-			boolean up = findMinHeight(field, x - 1, y, cluster, set);
+	public static void searchMineral(char[][] field, int x, int y) {
+		if(x - 1 >= 0 && field[x - 1][y] == 'x') {
+			List<Pos> cluster = new ArrayList<>();
+			boolean up = findMinHeight(field, x - 1, y, cluster);
 			if(up == true) {
-				fallCluster(field, set, cluster);
+				fallCluster(field, cluster);
 				return;
 			}
 		}
 		
-		if(x + 1 < field.length && field[x + 1][y].equals("x")) {
-			Set<Pos> cluster = new HashSet<>();
-			Set<Pos> set = new HashSet<>();
-			boolean down = findMinHeight(field, x + 1, y, cluster, set);
+		if(x + 1 < field.length && field[x + 1][y] == 'x') {
+			List<Pos> cluster = new ArrayList<>();
+			boolean down = findMinHeight(field, x + 1, y, cluster);
 			if(down == true) {
-				fallCluster(field, set, cluster);
+				fallCluster(field, cluster);
 				return;
 			}
 		}
 
-		if(y - 1 >= 0 && field[x][y - 1].equals("x")) {
-			Set<Pos> cluster = new HashSet<>();
-			Set<Pos> set = new HashSet<>();
+		if(y - 1 >= 0 && field[x][y - 1] == 'x') {
+			List<Pos> cluster = new ArrayList<>();
 			//System.out.println("왼쪽");
-			boolean left = findMinHeight(field, x, y - 1, cluster, set);
+			boolean left = findMinHeight(field, x, y - 1, cluster);
 			//System.out.println("left: " + left);
 			if(left == true) {
-				fallCluster(field, set, cluster);
+				fallCluster(field, cluster);
 				return;
 			}
 		}
 
-		if(y + 1 < field[0].length && field[x][y + 1].equals("x")) {
-			Set<Pos> cluster = new HashSet<>();
-			Set<Pos> set = new HashSet<>();
-			boolean right = findMinHeight(field, x, y + 1,cluster, set);
+		if(y + 1 < field[0].length && field[x][y + 1] == 'x') {
+			List<Pos> cluster = new ArrayList<>();
+			boolean right = findMinHeight(field, x, y + 1,cluster);
 			if(right == true) {
-				fallCluster(field, set, cluster);
+				fallCluster(field, cluster);
 				return;
 			}
 		}
@@ -114,64 +112,77 @@ public class Main {
 		return;
 	}
 
-	public static boolean findMinHeight(String[][] field, int x, int y, Set<Pos> cluster, Set<Pos> set) {
+	public static boolean findMinHeight(char[][] field, int x, int y, List<Pos> cluster) {
 		if(cluster.contains(new Pos(x, y))) return true;
 		if(x < 0 || x >= field.length || y < 0 || y >= field[0].length 
-		|| field[x][y].equals(".")) return true;
+		|| field[x][y] == '.') return true;
 		if(x == field.length - 1) {
 			//System.out.println("땅닿음");
 			return false;
 		}
 
-		if(x + 1 < field.length && field[x + 1][y].equals(".")) {
-			set.add(new Pos(x, y));
-		}
-
 		cluster.add(new Pos(x, y));
-		if(x - 1 >= 0 && field[x - 1][y].equals("x")) {
-			if(findMinHeight(field, x - 1, y, cluster, set) == false) return false;
+		if(x - 1 >= 0 && field[x - 1][y] == 'x') {
+			if(findMinHeight(field, x - 1, y, cluster) == false) return false;
 		}
 		
-		if(x + 1 < field.length && field[x + 1][y].equals("x")) {
-			if(findMinHeight(field, x + 1, y, cluster, set) == false) return false;
+		if(x + 1 < field.length && field[x + 1][y] == 'x') {
+			if(findMinHeight(field, x + 1, y, cluster) == false) return false;
 		}
 		
-		if(y - 1 >= 0 && field[x][y - 1].equals("x")) {
-			if(findMinHeight(field, x, y - 1, cluster, set) == false) return false;
+		if(y - 1 >= 0 && field[x][y - 1] == 'x') {
+			if(findMinHeight(field, x, y - 1, cluster) == false) return false;
 		}
 
-		if(y + 1 < field[0].length && field[x][y + 1].equals("x")) {
-			if(findMinHeight(field, x, y + 1, cluster, set) == false) return false;
+		if(y + 1 < field[0].length && field[x][y + 1] == 'x') {
+			if(findMinHeight(field, x, y + 1, cluster) == false) return false;
 		}
 		return true;
 	}
 	
-	public static void fallCluster(String[][] field, Set<Pos> set, Set<Pos> cluster) {
+	public static void fallCluster(char[][] field, List<Pos> cluster) {
+		List<Pos> bottom = new ArrayList<>();
+		Collections.sort(cluster, new Comparator<Pos>() {
+			@Override
+			public int compare(Pos o1, Pos o2) {
+				if(o1.y == o2.y) return o2.x - o1.x;
+				return o1.y - o2.y;
+			}
+		});
+		
+		bottom.add(new Pos(cluster.get(0).x, cluster.get(0).y));
+		int cur = cluster.get(0).y;
+		for(int i = 1; i < cluster.size(); i++) {
+			if(cluster.get(i).y != cur) {
+				bottom.add(new Pos(cluster.get(i).x, cluster.get(i).y));
+				cur = cluster.get(i).y;
+			}
+		}
+		
 		int min = 101;
-		Iterator<Pos> iter = set.iterator();
-		while(iter.hasNext()) {
-			Pos tmp = iter.next();
-			int dis = getDistance(field, tmp.x, tmp.y, cluster);
+		for(int i = 0; i < bottom.size(); i++) {
+			int dis = getDistance(field, bottom.get(i).x, bottom.get(i).y);
 			if(dis < min) min = dis;
 		}
 		
-		Iterator<Pos> iter2 = set.iterator();
-		while(iter2.hasNext()) {
-			Pos tmp = iter2.next();
-			int tmpX = tmp.x;
-			while(tmpX >= 0 && field[tmpX][tmp.y].equals("x") && cluster.contains(new Pos(tmpX, tmp.y))) {
-				field[tmpX][tmp.y] = ".";
-				field[tmpX + min][tmp.y] = "x";
+		for(int i = 0; i < bottom.size(); i++) {
+			int tmpX = bottom.get(i).x;
+			int tmpY = bottom.get(i).y;
+			while(tmpX >= 0) {
+				if(field[tmpX][tmpY] == 'x' && cluster.contains(new Pos(tmpX, tmpY))) {
+					field[tmpX][tmpY] = '.';
+					field[tmpX + min][tmpY] = 'x';
+				}
 				tmpX--;
 			}
 		}
 	}
 	
-	public static int getDistance(String[][] field, int x, int y, Set<Pos> cluster) {
+	public static int getDistance(char[][] field, int x, int y) {
 		int a = x + 1;
 		int b = y;
 		while(a < field.length) {
-			if(field[a][b].equals("x") && !cluster.contains(new Pos(a, b))) {
+			if(field[a][b] == 'x') {
 				break;
 			}
 			a++;
@@ -179,7 +190,7 @@ public class Main {
 		return a - x - 1;
 	}
 	
-	public static void printField(String[][] field) {
+	public static void printField(char[][] field) {
 		for(int i = 0; i < field.length; i++) {
 			for(int j = 0; j < field[0].length; j++) {
 				System.out.print(field[i][j]);
