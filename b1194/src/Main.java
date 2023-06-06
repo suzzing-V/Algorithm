8a61cf103018ff3e4b8928e63384389a4bc9c61c
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-06-06 21:05:12 +0900

    2 성공 BFS, 비트마스킹 이용

--- b1194/src/Main.java
+++ b1194/src/Main.java
@@ -5,15 +5,17 @@ public class Main {
 	public static class Pos {
 		int x;
 		int y;
-		public Pos(int x, int y) {
+		int count;
+		int key;
+		public Pos(int x, int y, int count, int key) {
 			this.x = x;
 			this.y = y;
+			this.count = count;
+			this.key = key;
 		}
 	}
-	
-	static List<Integer> routine = new ArrayList<>();
+
 	static int[][] dir = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-	static Set<Integer> key = new HashSet<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
@@ -21,100 +23,53 @@ public class Main {
 		int m = Integer.parseInt(st.nextToken());
 		
 		char[][] maze = new char[n][m];
-		Pos start = new Pos(0, 0);
+		Pos start = new Pos(0, 0, 0, 0);
 		for(int i = 0; i < n; i++) {
 			char[] line = bf.readLine().toCharArray();
 			for(int j = 0; j < m; j++) {
 				maze[i][j] = line[j];
 				if(maze[i][j] == '0') {
-					start = new Pos(i, j);
+					start = new Pos(i, j, 0, 0);
 					maze[i][j] = '.';
 				}
 			}
 		}
 		
-		fourDFS(maze, start.x, start.y, 0);
-		if(routine.isEmpty()) System.out.println("-1");
-		else {
-			Collections.sort(routine);
-			System.out.println(routine.get(0));
-		}
+		System.out.println(BFS(maze, start.x, start.y, start.count, start.key));
 	}
 	
-	public static void fourDFS(char[][] maze, int x, int y, int count) {
-		for(int i = 0; i < 4; i++) {
-			int mx = x + dir[i][0];
-			int my = y + dir[i][1];
-			count++;
-			if(mx < 0 || mx >= maze.length || my < 0 || my >= maze[0].length || maze[mx][my] == '#') {
-				count--;
-				continue;
-			}
-			//System.out.println("mx my: " + mx + " " + my);
-			if(maze[mx][my] == '.') {
-				threeDFS(maze, mx, my, i, count);
-			} else if(maze[mx][my] >= 'a' && maze[mx][my] <= 'f') {
-				//System.out.println(maze[mx][my] + "key 획득");
-				char tmp = maze[mx][my];
-				key.add(maze[mx][my] - 32);
-				maze[mx][my] = '.';
-				fourDFS(maze, mx, my, count);
-				key.remove(tmp - 32);
-				//System.out.println((tmp - 32) + "삭제");
-				maze[mx][my] = tmp;
-			} else if(maze[mx][my] >= 'A' && maze[mx][my] <= 'F') {
-				if(key.contains((int)maze[mx][my])) {
-					threeDFS(maze, mx, my, i, count);
-				} else {
-					count--;
+	public static int BFS(char[][] maze, int x, int y, int count, int key) {
+		Queue<Pos> queue = new LinkedList<>();
+		boolean[][][] visit = new boolean[maze.length][maze[0].length][64];
+		queue.add(new Pos(x, y, count, key));
+		
+		while(!queue.isEmpty()) {
+			Pos tmp = queue.poll();
+			//System.out.println("queue: " + tmp.x + " " + tmp.y + " " + tmp.count);
+			for(int i = 0; i < 4; i++) {
+				int mx = tmp.x + dir[i][0];
+				int my = tmp.y + dir[i][1];
+				if(mx < 0 || mx >= maze.length || my < 0 || my >= maze[0].length
+						|| maze[mx][my] == '#' || visit[mx][my][tmp.key]) {
 					continue;
 				}
-			} else if(maze[mx][my] == '1') {
-				//System.out.println("count: " + count);
-				routine.add(count);
-				return;
-			}
-			count --;
-		}
-	}
-	
-	public static void threeDFS(char[][] maze, int x, int y, int d, int count) {
-		int nx = x - dir[d][0];
-		int ny = y - dir[d][1];
-		for(int i = 0; i < 4; i++) {
-			int mx = x + dir[i][0];
-			int my = y + dir[i][1];
-			count++;
-			if(mx < 0 || mx >= maze.length || my < 0 || my >= maze[0].length
-					|| (mx == nx && my == ny) || maze[mx][my] == '#') {
-				count--;
-				continue;
-			}
-			//System.out.println("mx my: " + mx + " " + my);
-			if(maze[mx][my] == '.') {
-				threeDFS(maze, mx, my, i, count);
-			} else if(maze[mx][my] >= 'a' && maze[mx][my] <= 'f') {
-				//System.out.println(maze[mx][my] + "key 획득");
-				key.add(maze[mx][my] - 32);
-				char tmp = maze[mx][my];
-				maze[mx][my] = '.';
-				fourDFS(maze, mx, my, count);
-				key.remove(tmp - 32);
-				//System.out.println((tmp - 32) + "삭제");
-				maze[mx][my] = tmp;
-			} else if(maze[mx][my] >= 'A' && maze[mx][my] <= 'F') {
-				if(key.contains((int)maze[mx][my])) {
-					threeDFS(maze, mx, my, i, count);
-				} else {
-					count--;
-					continue;
+				//System.out.println("mx my: " + mx + " " + my);
+				if(maze[mx][my] == '1') return tmp.count + 1;
+				if(maze[mx][my] >= 'a' && maze[mx][my] <= 'f') {
+					int tmpKey = tmp.key | (1 << (maze[mx][my] - 'a'));
+					queue.add(new Pos(mx, my, tmp.count + 1, tmpKey));
+					visit[mx][my][tmpKey] = true; 
+				} else if(maze[mx][my] >= 'A' && maze[mx][my] <= 'F') {
+					if((tmp.key & 1 << (maze[mx][my] - 'A')) != 0) {
+						queue.add(new Pos(mx, my, tmp.count + 1, tmp.key));
+						visit[mx][my][tmp.key] = true;;
+					} 
+				} else if(maze[mx][my] == '.') {
+					queue.add(new Pos(mx, my, tmp.count + 1, tmp.key));
+					visit[mx][my][tmp.key] = true; 
 				}
-			} else if(maze[mx][my] == '1') {
-				//System.out.println("count: " + count);
-				routine.add(count);
-				return;
 			}
-			count --;
 		}
+		return -1;
 	}
 }
