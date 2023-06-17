132db6e119a5fac0d0464bf6a37e2413e58bcacb
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-06-17 22:40:49 +0900

    2 틀렸습니다

--- p입국심사/src/Solution.java
+++ p입국심사/src/Solution.java
@@ -1,60 +1,28 @@
+import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        int[] rest = new int[times.length];
-        n -= times.length;
-        for(int i = 0; i < times.length; i++) rest[i] = times[i];
+        Arrays.sort(times);
+        long start = times[0];
+        long end = times[times.length - 1] * n;
+        long mid = 0;
+        long possible = 0;
         
-        int save = -1;
-        while(n >= 0 && !isVoid(rest)) {
-            timeGoes(rest);
-            while(n > 0 && isContainsZero(rest)) {
-                if(save < 0) save = getMinIndex(rest, times);
-                if(save >= 0 && rest[save] == 0) {
-                    rest[save] = times[save];
-                    n --;
-                    save = -1;
-                } else break;
-            }
-            answer++;
+        while(start != end) {
+            mid = (start + end) / 2;
+            possible = getPossibleNum(mid, times);
+            
+            if(possible < n) start = mid + 1;
+            else end = mid - 1;
         }
+        
+        long answer = start;
         return answer;
     }
     
-    public void timeGoes(int[] rest) {
-        for(int i = 0; i < rest.length; i++)
-            if(rest[i] > 0) rest[i] --;
-    }
-    
-    public boolean isContainsZero(int[] rest) {
-        for(int i = 0; i < rest.length; i++) {
-            if(rest[i] == 0) return true;
-        }
-        return false;
-    }
-    
-    public boolean isVoid(int[] rest) {
-        for(int i = 0; i < rest.length; i++) {
-            if(rest[i] != 0) return false;
-        }
-        return true;
-    }
-    
-    public int getMinIndex(int[] rest, int[] times) {
-        int min = rest[0] + times[0];
-        int minIndex = 0;
-        for(int i = 0; i < rest.length; i++) {
-            if(min > rest[i] + times[i]) {
-                min = rest[i] + times[i];
-                minIndex = i;
-            } else if (min == rest[i] + times[i]) {
-                if(rest[minIndex] > rest[i]) {
-                    min = rest[i] + times[i];
-                    minIndex = i;
-                }
-            }
-        }
-        
-        return minIndex;
+    public long getPossibleNum(long mid, int[] times) {
+        long sum = 0;
+        for(int i = 0; i < times.length; i++) sum += mid / times[i];
+        return sum;
     }
 }
