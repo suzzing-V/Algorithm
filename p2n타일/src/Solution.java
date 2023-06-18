1504aabb638a5ef5537ea6e8cdb6a6cd8a3d586d
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-06-18 18:53:49 +0900

    2 성공 DP사용

--- p2n타일/src/Solution.java
+++ p2n타일/src/Solution.java
@@ -1,28 +1,17 @@
 class Solution {
+    static int R = 1000000007;
     public int solution(int n) {
-        int answer = 0;
-        long two = (long)(n / 2);
-        long one;
-        long R = 1000000007;
-        long count = 0;
-        if(n % 2 == 0) one = 0;
-        else one = 1;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
         
-        while(two >= 0) {
-            count += (factorial(two + one) / (factorial(two) * factorial(one))) % R;
-            two --;
-            one += 2;
-        }
-        count %= R;
-        answer = (int)count;
-        return answer;
-    }
-    
-    public long factorial(long n) {
-        if(n == 0 || n == 1) {
-            return 1;
-        }
+        long d1 = 1;
+        long d2 = 2;
         
-        return n * factorial(n - 1);
+        for(int i = 3; i <= n; i++) {
+            long d3 = (d1 % R + d2 % R) % R;
+            d1 = d2;
+            d2 = d3;
+        }
+        return (int) d2;
     }
 }
