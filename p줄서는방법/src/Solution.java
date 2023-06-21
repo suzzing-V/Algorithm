95f3943656d2762f560dce841d4bad582cf76060
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-06-21 13:56:56 +0900

    1 런타임에러

---
+++ p줄서는방법/src/Solution.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    long[] facto = new long[21];
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++) list.add(i);
+        
+        n -= 1;
+        int idx = 0;
+        while(true) {
+            long fac = factorial(n);
+            if(k % fac == 0) {
+                answer[idx++] = list.get((int)(k / fac) - 1);
+                list.remove(k / fac - 1);
+                putAnswer(list, answer, idx);
+                break;
+            } else {
+                answer[idx++] = list.get((int)(k / fac));
+                list.remove(k / fac);
+                if(fac == 2) {
+                    answer[idx++] = list.get(0);
+                    answer[idx++] = list.get(1);
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+    
+    public long factorial(int n) {
+        if(n == 0 || n == 1) return 1;
+        if(facto[n] != 0) return facto[n];
+        facto[n] = n * factorial(n - 1);
+        return facto[n];
+    }
+    
+    public void putAnswer(List<Integer> list, int[] answer, int idx) {
+        int k = 1;
+        for(int i = idx; i < answer.length; i++) {
+            answer[i] = list.get(list.size() - k);
+            k++;
+        }
+    }
+}
