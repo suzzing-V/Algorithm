a7a94ac39f879103228a6a6064cd1e95b54f93db
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-06-23 16:38:35 +0900

    5 성공 다이스트라 사용

--- p배달/src/Solution.java
+++ p배달/src/Solution.java
@@ -1,56 +1,52 @@
+import java.util.*;
+
 class Solution {
+    public class Info implements Comparable<Info> {
+        int cost;
+        int town;
+        Info(int town, int cost) {
+            this.cost = cost;
+            this.town = town;
+        }
+        
+        @Override
+        public int compareTo(Info info) {
+            if(this.cost > info.cost) return 1;
+            else if(this.cost < info.cost) return -1;
+            return 0;
+        }
+    }
+    
     boolean[] town = new boolean[51];
     public int solution(int N, int[][] road, int K) {
-        boolean[] visit = new boolean[N + 1];
-        visit[1] = true;
-        DFS(1, road, K, 0, visit);
-        int answer = countTown(town);
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
+        BFS(dist, 1, road);
+        int answer = 0;
+        for(int i = 1; i < dist.length; i++) {
+            if(dist[i] <= K) answer ++;
+        }
         return answer;
     }
     
-    public void DFS(int n, int[][] road, int k, int count, boolean[] visit) {
-        if(count > k || checkVisit(visit)) {
-            putTown(visit, town, n);
-            return;
-        }
+    public void BFS(int[] dist, int start, int[][] road) {
+        PriorityQueue<Info> pq = new PriorityQueue<>();
+        pq.add(new Info(start, 0));
         
-        if(checkVisit(visit)) {
-            for(int i = 0; i < visit.length; i++) {
-                town[i] = true;
+        while(!pq.isEmpty()) {
+            Info tmp = pq.poll();
+            
+            if(tmp.cost > dist[tmp.town]) continue;
+            for(int i = 0; i < road.length; i++) {
+                if(road[i][0] == tmp.town && tmp.cost + road[i][2] < dist[road[i][1]]) {
+                    dist[road[i][1]] = tmp.cost + road[i][2];
+                    pq.add(new Info(road[i][1], tmp.cost + road[i][2]));
+                } else if(road[i][1] == tmp.town && tmp.cost + road[i][2] < dist[road[i][0]]) {
+                    dist[road[i][0]] = tmp.cost + road[i][2];
+                    pq.add(new Info(road[i][0], tmp.cost + road[i][2]));
+                }
             }
-        }
-        
-        for(int i = 0; i < road.length; i++) {
-            if(road[i][0] == n && !visit[road[i][1]]) {
-                visit[road[i][1]] = true;
-                DFS(road[i][1], road, k, count + road[i][2], visit);
-                visit[road[i][1]] = false;
-            } else if(road[i][1] == n && !visit[road[i][0]]) {
-                visit[road[i][0]] = true;
-                DFS(road[i][0], road, k, count + road[i][2], visit);
-                visit[road[i][0]] = false;
-            }
-        }
-    }
-                               
-    public int countTown(boolean[] visit) {
-        int count = 0;
-        for(int i = 1; i < visit.length; i++) {
-            if(visit[i]) count++;
-        }
-        return count;
-    }
-    
-    public boolean checkVisit(boolean[] visit) {
-        for(int i = 1; i < visit.length; i++) {
-            if(!visit[i]) return false;
-        }
-        return true;
-    }
-    
-    public void putTown(boolean[] visit, boolean[] town, int n) {
-        for(int i = 0; i < visit.length; i++) {
-            if(visit[i] && i != n) town[i] = true;
         }
     }
 }
