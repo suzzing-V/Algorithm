9c1fd9a4deee4854585ea4c62872acbd8f1ef231
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-06-23 14:53:23 +0900

    4 틀렸습니다

--- p배달/src/Solution.java
+++ p배달/src/Solution.java
@@ -1,20 +1,34 @@
 class Solution {
+    boolean[] town = new boolean[51];
     public int solution(int N, int[][] road, int K) {
         boolean[] visit = new boolean[N + 1];
+        visit[1] = true;
         DFS(1, road, K, 0, visit);
-        int answer = countTown(visit);
+        int answer = countTown(town);
         return answer;
     }
     
     public void DFS(int n, int[][] road, int k, int count, boolean[] visit) {
-        if(count > k || checkVisit(visit)) return;
+        if(count > k || checkVisit(visit)) {
+            putTown(visit, town, n);
+            return;
+        }
         
-        visit[n] = true;
+        if(checkVisit(visit)) {
+            for(int i = 0; i < visit.length; i++) {
+                town[i] = true;
+            }
+        }
+        
         for(int i = 0; i < road.length; i++) {
-            if(road[i][0] == n) {
+            if(road[i][0] == n && !visit[road[i][1]]) {
+                visit[road[i][1]] = true;
                 DFS(road[i][1], road, k, count + road[i][2], visit);
-            } else if(road[i][1] == n) {
+                visit[road[i][1]] = false;
+            } else if(road[i][1] == n && !visit[road[i][0]]) {
+                visit[road[i][0]] = true;
                 DFS(road[i][0], road, k, count + road[i][2], visit);
+                visit[road[i][0]] = false;
             }
         }
     }
@@ -32,5 +46,11 @@ class Solution {
             if(!visit[i]) return false;
         }
         return true;
+    }
+    
+    public void putTown(boolean[] visit, boolean[] town, int n) {
+        for(int i = 0; i < visit.length; i++) {
+            if(visit[i] && i != n) town[i] = true;
+        }
     }
 }
