6d5d3b1b573aca784fc676f82dcb2d78310645a1
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-06-29 22:00:26 +0900

    1 성공

---
+++ p예상대진표/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int round = 1;
+        while(!(Math.abs(a - b) == 1 && Math.min(a, b) % 2 == 1 && Math.max(a, b) % 2 == 0)) {
+            if(a % 2 == 0) a /= 2;
+            else a = (a + 1) / 2;
+            if(b % 2 == 0) b /= 2;
+            else b = (b + 1) / 2;
+            round++;
+        }
+        return round;
+    }
+}
