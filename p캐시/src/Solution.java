32669b06aca2c0dd87a254e35aeb67fe7e2755fe
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-07-13 02:16:57 +0900

    2 성공

--- p캐시/src/Solution.java
+++ p캐시/src/Solution.java
@@ -4,22 +4,22 @@ class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
         LinkedList<String> link = new LinkedList<>();
-        
+
         for(int i = 0; i < cities.length; i++) {
             String city = changeToSmall(cities[i]);
             if(link.contains(city)) {
                 answer ++;
-                if(!link.isEmpty() && link.size() == cacheSize) link.removeLast();
+                link.remove(city);
                 link.addFirst(city);
             } else {
                 answer += 5;
                 if(!link.isEmpty() && link.size() == cacheSize) link.removeLast();
-                link.addFirst(city);
+                if(link.size() < cacheSize) link.addFirst(city);
             }
         }
         return answer;
     }
-    
+
     public String changeToSmall(String str) {
         String result = "";
         for(int i = 0; i < str.length(); i++) {
