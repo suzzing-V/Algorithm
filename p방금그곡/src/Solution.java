f0cd351a45c0ecbb5e85ade2428bac8b5d401b98
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-08-09 15:59:27 +0900

    1 정답

--- p방금그곡/src/Solution.java
+++ p방금그곡/src/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(String m, String[] musicinfos) {
-        
+
         String answer = "";
         int max = 0;
-        
+
         m = changeSharp(m);
         for(int i = 0;i < musicinfos.length; i++) {
             String[] split = new String[4];
             split = musicinfos[i].split(",");
-            
+
             int l = calculateMusicLength(split[0], split[1]);
             String title = split[2];
             String melody = changeSharp(split[3]);
@@ -17,14 +17,14 @@ class Solution {
                 max = l;
             }
         }
-        
+
         if(answer.equals("")) return "(None)";
         return answer;
     }
-    
+
     public String changeSharp(String str) {
         String result = "";
-        
+
         for(int i = 0; i < str.length(); i++) {
             if(i != str.length() - 1 && str.charAt(i + 1) == '#') {
                 result += String.valueOf((char)(str.charAt(i) + 32));
@@ -35,29 +35,29 @@ class Solution {
         }
         return result;
     }
-    
+
     public int calculateMusicLength(String start, String end) {
-        
+
         String[] split = new String[2];
         split = start.split(":");
         int startMinute = Integer.parseInt(split[0]);
         int startSecond = Integer.parseInt(split[1]);
-        
+
         split = end.split(":");
         int endMinute = Integer.parseInt(split[0]);
         int endSecond = Integer.parseInt(split[1]);
-        
-        
+
+
         int minute = endMinute - startMinute;
         if(startSecond > endSecond) {
             endSecond += 60;
             minute --;
         }
         int second = endSecond - startSecond;
-        
+
         return minute * 60 + second;
     }
-    
+
     public boolean isRight(String m, int l, String melody) {
         int i = 0;
         while(l > 0 && i < melody.length()) {
@@ -65,10 +65,10 @@ class Solution {
             l--;
             i++;
         }
-        
+
         return false;
     }
-    
+
     public boolean isSame(String m, String melody, int i, int l) {
         int mIndex = 0;
         while(mIndex < m.length() && l > 0) {
@@ -78,7 +78,7 @@ class Solution {
             i++;
             if(i == melody.length()) i = 0;
         }
-        
+
         if(mIndex == m.length()) return true;
         return false;
     }
