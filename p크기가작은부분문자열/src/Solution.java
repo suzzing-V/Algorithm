163f1877bd3071722415df1363a9d121b4317b7a
sjlim1999@naver.com <sjlim1999@naver.com>
2023-08-29 15:10:53 +0900

    성공

--- p크기가작은부분문자열/src/Solution.java
+++ p크기가작은부분문자열/src/Solution.java
@@ -1,5 +1,14 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.*;
+
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        long pnum = Long.parseLong(p);
+        
+        for(int i = 0; i < t.length() - (p.length() - 1); i++) {
+            Long num = Long.parseLong(t.substring(i, i + p.length()));
+            if(num <= pnum) answer++;
+        }
+        return answer;
     }
 }
