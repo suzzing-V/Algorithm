2d88ef2f069054560ad81059df2d4f6825f11f32
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-10-05 23:58:59 +0900

    성공

--- b9506/Main.java
+++ b9506/Main.java
@@ -1,9 +1,10 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String line = "";
         while(!(line = bf.readLine()).equals("-1")) {
@@ -12,6 +13,25 @@ public class Main {
     }
 
     public static void isPerfect(int num) {
+        int sum = 0;
+        List<Integer> list = new ArrayList<>();
 
+        for(int i = 1; i < num; i++) {
+            if(num % i == 0) {
+                sum += i;
+                list.add(i);
+            }
+        }
+
+        if(sum == num) {
+            System.out.print(num + " = ");
+            for(int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i));
+                if(i != list.size() - 1) System.out.print(" + ");
+            }
+            System.out.println();
+        } else {
+            System.out.println(String.valueOf(num) + " is NOT perfect.");
+        }
     }
 }
