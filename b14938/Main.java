6b468a310694d4dffe08bfd6152e4d024dc943af
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-02-29 23:41:57 +0900

    초기

---
+++ b14938/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
