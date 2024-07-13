fa021e8ee59f073fa0fa497b1761d7eaafbff785
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-13 23:58:29 +0900

    b4344

---
+++ b4344/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int c = Integer.parseInt(bf.readLine());
+
+        for(int i = 0; i < c; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < n; j++) {
+
+            }
+        }
+    }
+}
