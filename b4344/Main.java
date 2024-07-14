b27b4c9d1bf6eda52cce61531efd3d26cf613b27
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-14 13:33:07 +0900

    b4344

--- b4344/Main.java
+++ b4344/Main.java
@@ -11,9 +11,24 @@ public class Main {
         for(int i = 0; i < c; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int n = Integer.parseInt(st.nextToken());
+            int total = 0;
+            int[] arr = new int[n];
             for(int j = 0; j < n; j++) {
-
+                arr[j] = Integer.parseInt(st.nextToken());
+                total += arr[j];
             }
+
+            double avg = (double)total/n;
+            int count = 0;
+            for(int j = 0; j < n; j++) {
+                if(arr[j] > avg) {
+                    count++;
+                }
+            }
+
+            double per = (double)count/n * 100;
+            per = Math.round(per*1000) / 1000.0;
+            System.out.println(String.format("%.3f", per) + "%");
         }
     }
 }
