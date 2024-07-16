255d73da47b07bbf5b54d772f636ecd97393fff6
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-16 23:53:47 +0900

    b30180

---
+++ b30180/Main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
