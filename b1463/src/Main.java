96e1d0a7542de54f9db4af59623f28d2c2062c69
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-08-28 01:48:41 +0900

    dp bottom up

--- b1463/src/Main.java
+++ b1463/src/Main.java
@@ -7,36 +7,21 @@ import java.io.OutputStreamWriter;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[N + 2];
-        arr[1] = 0; arr[2] = 1;
-        for(int i = 3; i <= N; i++) {
-            fillArr(arr, i);
+        int[] dp = new int[1000001];
+        dp[1] = 0;
+        for(int i = 2; i <= N; i++) {
+            if(i % 2 == 0 && i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 2], dp[i / 3]) + 1;
+            } else if(i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
+            } else if(i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
+            } else {
+                dp[i] = dp[i - 1] + 1;
+            }
         }
-        bw.write(Integer.toString(arr[N]));
-        bw.close();
-    }
 
-    public static void fillArr(int[] arr, int i) {
-        int three = 0; int two = 0;
-        int minus = i - 1;
-        if(i % 3 == 0) {
-            three = i / 3;
-        }
-        if(i % 2 == 0) {
-            two = i / 2;
-        }
-        if(three == 0 && two != 0) {
-            arr[i] = Math.min(arr[two], arr[minus]) + 1;
-        } else if ( three != 0 && two == 0) {
-            arr[i] = Math.min(arr[three], arr[minus]) + 1;
-        } else if (three != 0 && two != 0) {
-            int min = Math.min(arr[three], arr[two]);
-            min = Math.min(min, arr[minus]);
-            arr[i] = min + 1;
-        } else {
-            arr[i] = arr[minus] + 1;
-        }
+        System.out.println(dp[N]);
     }
 }
