b8065bf620cae0f3a839350e0a71029c13597870
suzzingv <sjlim1999@naver.com>
2024-09-08 23:56:54 +0900

    s

---
+++ b15486/Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
