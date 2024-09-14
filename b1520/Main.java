224d153a384eaee0525de699ad52f6281fbe5034
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-09-15 01:45:43 +0900

    b1520

--- b1520/Main.java
+++ b1520/Main.java
@@ -10,6 +10,39 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        dp = new int[n][m];
 
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for(int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(dfs(0, 0));
+    }
+
+    public static int dfs(int x, int y) {
+        if(x == n - 1 && y == m - 1) {
+            return 1;
+        }
+
+        if(dp[x][y] != -1) {
+            return dp[x][y];
+        }
+
+        dp[x][y] = 0;
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+            if(nx <= n - 1 && nx >= 0 && ny <= m - 1 && ny >= 0 && map[nx][ny] < map[x][y]) {
+                dp[x][y] += dfs(nx, ny);
+            }
+        }
+        return dp[x][y];
     }
 }
