b5febbd767df60a8adc91d97b5cc45ef6c260f6f
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-09-29 22:39:54 +0900

    b7579 홀리몰리!!!!첫!!!

--- b7579/Main.java
+++ b7579/Main.java
@@ -4,13 +4,42 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        long[] dp = new long[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 2; i < n + 1; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][10001];
+        int[] mem = new int[n + 1];
+        int[] c = new int[n + 1];
+
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 1; i <= n; i++) {
+            mem[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(dp[n]);
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 1; i <= n; i++) {
+            c[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= 10000; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        for(int i = 1; i <= n; i++) {
+                dp[i][c[i]] = Math.max(dp[i - 1][c[i]], mem[i]);
+            for(int j = 0; j <= 10000; j++) {
+                if(dp[i - 1][j] != -1) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+                    dp[i][c[i] + j] = Math.max(Math.max(dp[i - 1][c[i] + j], mem[i] + dp[i - 1][j]), dp[i][c[i] + j]);
+                }
+            }
+        }
+
+        for(int i = 0; i <= 10000; i++) {
+            if(dp[n][i] >= m) {
+                System.out.println(i);
+                break;
+            }
+        }
     }
 }
