ba40453bc62e9894f904547f75e2ce9ca1138ab0
suzzingV <sjlim1999@naver.com>
2025-01-20 13:56:02 +0900

    b9655

---
+++ b9655/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n % 2 != 0) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+    }
+}
