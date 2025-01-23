e144ebd3d843f6f0cf981a85312a40b606bcbf5f
suzzingV <sjlim1999@naver.com>
2025-01-23 18:00:06 +0900

    b1406

---
+++ b1406/Main.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+        String str = br.readLine();
+        for(int i = 0; i < str.length(); i++) {
+            left.add(str.charAt(i));
+        }
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) {
+//            System.out.println("cursor: " + cursor);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if(command.equals("P")) {
+                left.add(st.nextToken().charAt(0));
+            } else if(command.equals("L")) {
+                if(!left.isEmpty()) right.add(left.pop());
+            } else if(command.equals("D")) {
+                if(!right.isEmpty()) left.add(right.pop());
+            } else if(command.equals("B")) {
+                if(!left.isEmpty()) left.pop();
+            }
+//            for (int j = 1; j < editer.size() - 1; j++) {
+//                System.out.print(editer.get(j));
+//            }
+//            System.out.println();
+        }
+
+        List<Character> list = new ArrayList<>(left);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i));
+        }
+        while(!right.isEmpty()) {
+            System.out.print(right.pop());
+        }
+    }
+}
