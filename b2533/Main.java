90d247dfd0d58f3fcbb2f6b3f0125bb528de05d8
suzzingv <sjlim1999@naver.com>
2025-03-15 17:19:31 +0900

    tree :

--- b2533/Main.java
+++ b2533/Main.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class Main {
 
+    private static int cnt = 0;
     private static List<Integer>[] friends;
     private static int n;
-    private static int[][] dp; // 현재 노드의 상태에 따른 현재 노드까지의 early 최소 개수
     private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -13,12 +13,10 @@ public class Main {
         BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(bf.readLine());
         friends = new ArrayList[n + 1];
-        dp = new int[n + 1][2];
         visited = new boolean[n + 1];
         for(int i = 0; i <= n ; i++) {
             friends[i] = new ArrayList<>();
         }
-
         for(int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int f1 = Integer.parseInt(st.nextToken());
@@ -28,19 +26,28 @@ public class Main {
         }
 
         dfs(1);
-        bw.write(String.valueOf(Math.min(dp[1][0], dp[1][1])));
+        bw.write(String.valueOf(cnt));
+//        System.out.println(Arrays.toString(isEarly));
         bw.close();
     }
 
-    private static void dfs(int curr) {
-        dp[curr][0] = 0; // 현재 노드가 early가 아닐 때 초기 early의 개수
-        dp[curr][1] = 1; // 현재 노드가 early일 때 초기 early의 개수
-        visited[curr] = true; // 밑으로만 내려가면서 탐색
+    private static int dfs(int curr) {
+        visited[curr] = true;
+        if(friends[curr].size() == 1 && curr != 1) { // 친구 하나인 경우가 무조건 리프노드는 아니다. 첫 노드가 친구 하나일 수도 있다. 따라서 첫 노드가 아니라는 조건을 붙여줘야 한다.
+            return 1;
+        }
+
+        int not_early = 0;
+//        System.out.println(curr);
         for(int f : friends[curr]) {
             if(visited[f]) continue;
-            dfs(f);
-            dp[curr][0] += dp[f][1]; // 현재 노드가 early가 아니면 주변 노드가 early여야 하므로 주변 노드 early인 경우에서의 최소 early수를 더한다.
-            dp[curr][1] += Math.min(dp[f][0], dp[f][1]); // 현재 노드가 early면 주변 노드가 early든 아니든 상관 없으므로 둘 중 최소값 저장한다.
+            not_early += dfs(f);
+//            System.out.println(f + " " + not_early);
         }
+        if(not_early != 0) {
+            cnt ++;
+            return 0;
+        }
+        return 1;
     }
 }
