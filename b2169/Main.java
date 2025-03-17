a807132e3d08586f14a2fedc8e0fcde541f2308b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2025-03-17 16:03:54 +0900

    dfs + dp

--- b2169/Main.java
+++ b2169/Main.java
@@ -6,7 +6,7 @@ public class Main {
     static int n;
     static int m;
     static int[][] map;
-    static int[][] dp;
+    static int[][][] dp;
     static boolean[][] visited;
     static int[][] dir = { {1, 0}, {0, 1}, {0, -1}};
 
@@ -17,54 +17,40 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new int[n + 1][m + 1];
-        dp = new int[n + 1][m + 1];
-        visited = new boolean[n][m];
+        dp = new int[n + 1][m + 1][3];
+        visited = new boolean[n + 1][m + 1];
 
         for(int i = 1; i <= n; i++) {
             st = new StringTokenizer(bf.readLine());
             for(int j = 1; j <= m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(i == 1) dp[i][j] = map[i][j] + dp[i][j - 1];
+                dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = -200000000;
             }
         }
 
-        for(int i = 2; i <= n; i++) {
-            int[] from_left = new int[m + 1];
-            int[] from_right = new int[m + 1];
-            from_left[1] = dp[i - 1][1] + map[i][1];
-            for(int j = 2; j <= m; j++) {
-                from_left[j] = Math.max(dp[i - 1][j], from_left[j - 1]) + map[i][j];
-            }
-
-            from_right[m] = dp[i - 1][m] + map[i][m];
-            for(int j = m - 1; j >= 1; j --) {
-                from_right[j] = Math.max(dp[i - 1][j], from_right[j + 1]) + map[i][j];
-            }
-
-            for(int j = 1; j <= m; j++) {
-                dp[i][j] = Math.max(from_left[j], from_right[j]);
-            }
-        }
-        bw.write(String.valueOf(dp[n][m]));
+        int max = dfs(1, 1, 0);
+        bw.write(String.valueOf(max));
         bw.close();
     }
 
-    private static int dfs(int x, int y) {
-        if(dp[x][y] != -1000) {
-            return dp[x][y];
+    private static int dfs(int x, int y, int from) { // dir: 0 - 위에서 1 - 왼쪽에서 2 - 오른쪽에서 옴
+        if(dp[x][y][from] != -200000000) {
+            return dp[x][y][from];
+        }
+        if(x == n && y == m) {
+            return map[x][y];
         }
 
         visited[x][y] = true;
-        System.out.println(x + " " + y);
-        int max = -1000;
+//        System.out.println(x + " " + y);
         for(int i = 0; i < 3; i++) {
             int nx = x + dir[i][0];
             int ny = y + dir[i][1];
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny]) continue;
+            if(nx <= 0 || ny <= 0 || nx > n || ny > m || visited[nx][ny]) continue;
 
-            dp[x][y] = Math.max(dp[x][y], dfs(nx, ny));
+            dp[x][y][from] = Math.max(dp[x][y][from], dfs(nx, ny, i));
         }
-
-        return dp[x][y] = map[x][y] + max;
+        visited[x][y] = false;
+        return dp[x][y][from] += map[x][y];
     }
 }
