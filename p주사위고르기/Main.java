bce5e4765865dadd84ee6fd7519bc2545610a51f
suzzingv <84590065+suzzingv@users.noreply.github.com>
2025-06-11 15:32:34 +0900

    누적합

--- p주사위고르기/Main.java
+++ p주사위고르기/Main.java
@@ -1,110 +1,83 @@
 import java.util.*;
 
 class Solution {
-    int diceNum;
-    int max = 0;
-    List<Integer> maxDices = new ArrayList<>();
 
-    public int[] solution(int[][] dice) {
-        diceNum = dice.length;
-        int[] answer = new int[diceNum / 2];
+    private int[] aSumCount = new int[601];
+    private int[] bPrefixSum = new int[601];
+    private int maxWin = 0;
+    private int[] answer;
+    private int n;
+    private int[][] dice;
 
-        List<Integer> selected = new ArrayList<>();
-        for(int i = 1; i <= diceNum; i++) {
-            selected.add(i);
-            selectDice(selected, 1, i, dice);
-            selected.remove(selected.size() - 1);
+    public int[] solution(int[][] dice1) {
+        n = dice1.length;
+        dice = dice1;
+        answer = new int[n / 2];
+        makeDiceCombi(0, new ArrayList<>());
+
+        for(int i = 0; i < n / 2; i++) {
+            answer[i] ++;
         }
-
-        for(int i = 0; i < diceNum / 2; i++) {
-            answer[i] = maxDices.get(i);
-        }
-
         return answer;
     }
 
-    public void selectDice(List<Integer> selected, int count, int start, int[][] dice) {
-        if(count == diceNum / 2) {
-            List<Integer> selectedB = new ArrayList<>();
-            for(int i = 1; i <= diceNum; i++) {
-                if(!selected.contains(i)) {
-                    selectedB.add(i);
+    private void makeDiceCombi(int dice, List<Integer> dicesA) {
+        if(dicesA.size() == n / 2) {
+            aSumCount = new int[601];
+            bPrefixSum = new int[601];
+            List<Integer> dicesB = new ArrayList<>();
+            for(int i = 0; i < n; i ++) {
+                if(!dicesA.contains(i)) dicesB.add(i);
+            }
+            makeDiceSum(0, 0, dicesA, aSumCount);
+            makeDiceSum(0, 0, dicesB, bPrefixSum);
+            makePrefixSum(bPrefixSum);
+
+            int win = 0;
+            for(int i = 1; i <= 600; i++) {
+                if(aSumCount[i] != 0) {
+                    win += aSumCount[i] * bPrefixSum[i - 1];; // a의 합이 i일 때, b의 합이 i보다 작아야 a가 승리
                 }
             }
 
-            int winCount = getWinCount(selected, selectedB, dice);
-            if(winCount > max) {
-                max = winCount;
-                maxDices = new ArrayList<>();
-                for(Integer num: selected) {
-                    maxDices.add(num);
-                }
+            if(win > maxWin) { // 해당 조합일 때 a의 승리횟수가 지금까지의 최대 승리횟수보다 크다면 갱신
+                maxWin = win;
+                answer = listToArray(dicesA);
             }
             return;
-
         }
 
-
-        for(int i = start + 1; i <= diceNum; i++) {
-            selected.add(i);
-            selectDice(selected, count + 1, i, dice);
-            selected.remove(selected.size() - 1);
+        for(int i = dice; i < n; i ++) {
+            dicesA.add(i);
+            makeDiceCombi(i + 1, dicesA);
+            dicesA.remove((int)dicesA.size() - 1);
         }
     }
 
-    public int getWinCount(List<Integer> selectedA, List<Integer> selectedB, int[][] dice) {
-        int[] aSum = new int[100000];
-        int[] bSum = new int[100000];
-
-        countSum(aSum, selectedA, dice);
-        countSum(bSum, selectedB, dice);
-
-        int winCount = 0;
-        for(int i = 1; i <= diceNum * 100; i++) {
-            if(aSum[i] != 0) {
-                int bSumCount = 0;
-                for(int j = 1; j < i; j ++) {
-                    bSumCount += bSum[j];
-                }
-                winCount += aSum[i] * bSumCount;
-            }
+    private void makeDiceSum(int idx, int sum, List<Integer> dices, int[] sumCount) {
+        if(idx == dices.size()) {
+            sumCount[sum] ++;
+            return;
         }
 
-        return winCount;
-    }
-
-    public void countSum(int[] sum, List<Integer> selected, int[][] dice) {
+        int d = dices.get(idx);
         for(int i = 0; i < 6; i ++) {
-            int first = dice[selected.get(0) - 1][i];
-            if(diceNum >= 4) {
-                for(int j = 0; j < 6; j++) {
-                    int second = dice[selected.get(1) - 1][j];
-                    if(diceNum >= 6) {
-                        for(int k = 0; k < 6; k++) {
-                            int third = dice[selected.get(2) - 1][k];
-                            if(diceNum >= 8) {
-                                for(int l = 0; l < 6; l++) {
-                                    int fourth = dice[selected.get(3) - 1][l];
-                                    if(diceNum == 10) {
-                                        for(int m = 0; m < 6; m++) {
-                                            int fifth = dice[selected.get(4) - 1][m];
-                                            sum[first + second + third + fourth + fifth] += 1;
-                                        }
-                                    } else {
-                                        sum[first + second + third + fourth] += 1;
-                                    }
-                                }
-                            } else {
-                                sum[first + second + third] += 1;
-                            }
-                        }
-                    } else {
-                        sum[first + second] += 1;
-                    }
-                }
-            } else {
-                sum[first] += 1;
-            }
+            makeDiceSum(idx + 1, sum + dice[d][i], dices, sumCount);
         }
+    }
+
+    private void makePrefixSum(int[] sumCount) {
+        for(int i = 1; i < 601; i++) {
+            sumCount[i] += sumCount[i - 1];
+        }
+    }
+
+    private int[] listToArray(List<Integer> list) {
+        int[] arr = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
     }
 }
