7c6fb02c529726061939902a2695463adc0b3ac0
suzzingv <84590065+suzzingv@users.noreply.github.com>
2025-06-12 23:32:19 +0900

    greedy

--- pn+1카드게임/Solution.java
+++ pn+1카드게임/Solution.java
@@ -1,74 +1,80 @@
 import java.util.*;
 
+// 새로운 카드들 다 모아놓고 필요할 때 그때 미리 사놓은 거로 치기
 class Solution {
-    List<Integer> myCards = new ArrayList<>();
-    int n;
+
+    private List<Integer> notPicked = new ArrayList<>();
+    private List<Integer> init = new ArrayList<>();
+    private int n;
 
     public int solution(int coin, int[] cards) {
         n = cards.length;
 
+        // 초기 카드 담기
         for(int i = 0; i < n / 3; i++) {
-            myCards.add(cards[i]);
+            init.add(cards[i]);
         }
 
-        List<Integer> picked = new ArrayList<>();
-        return game(n / 3, coin, 1, picked, cards);
-    }
+        // 매 라운드마다 동전 가장 적게 사용하는 쪽 선택하기
+        int idx = n / 3;
+        int round = 0;
 
-    public int game(int idx, int coin, int round, List<Integer> picked, int[] cards) {
-        if(idx >= n) {
-            return round;
-        }
+        while(true) {
+            round ++;
 
-        int first = cards[idx];
-        int second = cards[idx + 1];
-
-        picked.add(first);
-        picked.add(second);
-        // 기존 페어 내기
-        for(int i = 0; i < myCards.size() - 1; i++) {
-            for(int j = i + 1; j < myCards.size(); j++) {
-                int a = myCards.get(i);
-                int b = myCards.get(j);
-                int sum = a + b;
-                if(sum == n + 1) {
-                    myCards.remove((Integer)a);
-                    myCards.remove((Integer)b);
-                    return game(idx + 2, coin, round + 1, picked, cards);
-                }
+            if(idx == n) { // 남은 카드뭉치가 없을 경우 게임 종료
+                break;
             }
-        }
+            // 뽑지 않은 카드 리스트에 새로운 카드 2개 추가
+            notPicked.add(cards[idx]);
+            notPicked.add(cards[idx + 1]);
 
-        if(coin >= 1) { // 카드 1장 뽑기
-            for(int i = 0; i < myCards.size(); i++) {
-                for(int j = 0; j < picked.size(); j++) {
-                    int a = myCards.get(i);
-                    int b = picked.get(j);
-                    int sum = a + b;
-                    if(sum == n + 1) {
-                        myCards.remove((Integer)a);
-                        picked.remove((Integer)b);
-                        return game(idx + 2, coin - 1, round + 1, picked, cards);
-                    }
-                }
-            }
-        }
+            // 코인 0개 사용 : 초기 카드에서 2개 뽑기
+            if(canMakeInOneList(init)) {
 
-        if(coin >= 2) { // 카드 2장 가지기
-            for(int i = 0; i < picked.size() - 1; i++) {
-                for(int j = i + 1; j < picked.size(); j++) {
-                    int a = picked.get(i);
-                    int b = picked.get(j);
-                    int sum = a + b;
-                    if(sum == n + 1) {
-                        picked.remove((Integer)a);
-                        picked.remove((Integer)b);
-                        return game(idx + 2, coin - 2, round + 1, picked, cards);
-                    }
-                }
+                // 코인 1개 사용 : 초기 카드에서 1개, 뽑지 않은 카드에서 1개 사용
+            } else if(canMakeInTwoList(init, notPicked)) {
+                coin -= 1;
+
+                // 코인 2개 사용 : 뽑지 않은 카드에서 2개 사용
+            } else if(canMakeInOneList(notPicked)) {
+                coin -= 2;
+
+                // 다 불가능하면 더이상 n + 1을 만들 수 없으므로 게임 종료
+            } else {
+                break;
             }
+
+            // 코인이 부족하면 게임 종료
+            if(coin < 0) break;
+
+            idx += 2;
         }
 
         return round;
+    }
+
+    private boolean canMakeInOneList(List<Integer> list) {
+        for(int i : list) {
+            if(list.contains(n + 1 - i)) {
+                list.remove((Integer)i);
+                list.remove((Integer)(n + 1 - i));
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean canMakeInTwoList(List<Integer> list1, List<Integer> list2) {
+        for(int i : list1) {
+            if(list2.contains(n + 1 - i)) {
+                list1.remove((Integer)i);
+                list2.remove((Integer)(n + 1 - i));
+                return true;
+            }
+        }
+
+        return false;
     }
 }
