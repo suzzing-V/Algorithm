c81913a9e318b6d8cffd8fbf43708e3bb9a90799
suzzingv <84590065+suzzingv@users.noreply.github.com>
2025-06-17 20:35:33 +0900

    dp

--- p아방가르드타일/Solution.java
+++ p아방가르드타일/Solution.java
@@ -1,20 +1,34 @@
 class Solution {
 
-    int MOD = 1000000007;
+    private long[] dp;
+    private int MOD = 1000000007;
+
     public int solution(int n) {
-        long[] dp = new long[n + 1];
-        if(n == 1) return 1;
-        if(n == 2) return 3;
-        if(n == 3) return 10;
+        dp = new long[100001];
         dp[1] = 1;
         dp[2] = 3;
         dp[3] = 10;
-        long[] add = {12, 2, 4};
+        dp[4] = dp[3] + dp[2] * 2 + dp[1] * 5 + 2;
+        dp[5] = dp[4] + dp[3] * 2 + dp[2] * 5 + dp[1] * 2 + 2;
+        dp[6] = dp[5] + dp[4] * 2 + dp[3] * 5 + dp[2] * 2 + dp[1] * 2 + 4;
 
-        for(int i = 4; i <= n; i++) {
-            dp[i] = (dp[i - 1] * 1 % MOD + dp[i - 2] * 2 % MOD + dp[i - 3] * 5 % MOD + add[i % 3]) % MOD;
-            add[i % 3] += (dp[i - 1] * 2 % MOD + dp[i - 2] * 2 % MOD + dp[i - 3] * 4 % MOD);
+        long[] sum = {dp[2] * 2 + dp[1] * 2, 0, dp[1] * 2}; // 3의 배수마다 추가로 더해지는 값이 반복된다.
+        int[] notSplit = {4, 2, 2};
+        for(int i = 7; i <= n; i++) {
+            dp[i] += notSplit[i % 3]; // 쪼개지지 않는 n * i 타일 만드는 경우의 수
+            dp[i] += dp[i - 1] % MOD;
+            dp[i] += dp[i - 2] * 2 % MOD;
+            dp[i] += dp[i - 3] * 5 % MOD;
+
+            // sum 갱신
+            sum[i % 3] += 2 * dp[i - 4] % MOD;
+            sum[i % 3] += 2 * dp[i - 5] % MOD;
+            sum[i % 3] += 4 * dp[i - 6] % MOD;
+
+            dp[i] += sum[i % 3];
+            dp[i] %= MOD;
         }
-        return (int)(dp[n]);
+
+        return (int)dp[n];
     }
 }
