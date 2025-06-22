89f7908458badec0366b8b707b3678f18af5377f
suzzingv <84590065+suzzingv@users.noreply.github.com>
2025-06-22 13:02:00 +0900

    bfs

--- p미로탈출명령어/Solution.java
+++ p미로탈출명령어/Solution.java
@@ -1,47 +1,81 @@
 import java.util.*;
 
 class Solution {
-    String fast = null;
-    int k;
-    int r;
-    int c;
-    int n;
-    int m;
-    int[][] dir = { {1, 0}, {0, -1}, {0, 1}, {-1, 0}};
-    String[] dirStr = {"d", "l", "r", "u"};
+
+    private int n;
+    private int m;
+    private char[][] map;
+    private int k;
+    private int r;
+    private int c;
+    private int[][] dir = { {1, 0}, {0, -1}, {0, 1}, {-1, 0}};
+    private String[] dirStr = {"d", "l", "r", "u"};
 
     public String solution(int n1, int m1, int x, int y, int r1, int c1, int k1) {
+        map = new char[n1 + 1][m1 + 1];
+        n = n1;
+        m = m1;
         k = k1;
         r = r1;
         c = c1;
-        n = n1;
-        m = m1;
 
-        int dist = Math.abs(x - r) + Math.abs(y - c);
-        if(dist > k) return "impossible"; // 갈 수 있는 거리가 최단 거리보다 작을 경우
-        if(dist % 2 != k % 2) return "impossible"; // 왔다갔다 했을 때 불가능한 경우
+        // 남은 거리가 도착지까지 도착하기 위한 최소 거리보다 적으면 안됨
+        if(distance(x, y, r, c) > k) return "impossible";
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(i == x && j == y) map[i][j] = 'S';
+                else if(i == r && j == c) map[i][j] = 'E';
+                else map[i][j] = '.';
+            }
+        }
 
-        dfs(x, y, new StringBuilder(""));
-        return fast;
+        return bfs(x, y);
     }
 
-    private void dfs(int x, int y, StringBuilder sb) {
-        if(fast != null) return;
-        if(sb.length() == k && x == r && y == c) { // 도착지 도달
-            fast = sb.toString();
-            return;
-        }
-        if((Math.abs(r - x) + Math.abs(c - y)) > k - sb.length()) { // 잔여 거리가 현재 위치에서의 최단 거리보다 작을 경우
-            return;
+    private String bfs(int sx, int sy) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(sx, sy, new StringBuilder()));
+
+        while(!pq.isEmpty()) {
+            Node curr = pq.remove();
+
+            // System.out.println(curr.x + " " + curr.y + " " + curr.route);
+            for(int i = 0; i < 4; i++) {
+                int nx = curr.x + dir[i][0];
+                int ny = curr.y + dir[i][1];
+                if(nx <= 0 || nx > n || ny <= 0 || ny > m || curr.route.toString().length() == k) continue;
+                if(distance(nx, ny, r, c) > k - curr.route.length() - 1) continue;
+                if(map[nx][ny] == 'E' && curr.route.length() + 1 == k) return curr.route.append(dirStr[i]).toString();
+                StringBuilder sb = new StringBuilder(curr.route.toString());
+                pq.add(new Node(nx, ny, sb.append(dirStr[i])));
+                break;
+            }
         }
 
-        for(int i = 0; i < 4; i++) {
-            int nx = x + dir[i][0];
-            int ny = y + dir[i][1];
-            if(nx <= 0 || ny <= 0 || nx > n || ny > m) continue;
-            sb.append(dirStr[i]);
-            dfs(nx, ny, sb);
-            if(sb.length() > 0) sb.deleteCharAt(sb.length() - 1);
+        return "impossible";
+    }
+
+    private int distance(int sx, int sy, int ex, int ey) {
+        return Math.abs(sx - ex) + Math.abs(sy - ey);
+    }
+
+    private class Node implements Comparable<Node> {
+        private int x;
+        private int y;
+        private StringBuilder route;
+
+        Node(int x, int y, StringBuilder route) {
+            this.x = x;
+            this.y = y;
+            this.route = route;
+        }
+
+        @Override
+        public int compareTo(Node n) {
+            if(this.route.length() != n.route.length()) {
+                return n.route.length() - this.route.length();
+            }
+            return this.route.toString().compareTo(n.route.toString());
         }
     }
 }
