c21a7e478b99b6cbdc8f8e28bf9f0fd0739b04b4
suzzingv <84590065+suzzingv@users.noreply.github.com>
2025-06-22 13:25:23 +0900

    greedy

--- p미로탈출명령어/Solution.java
+++ p미로탈출명령어/Solution.java
@@ -2,80 +2,32 @@ import java.util.*;
 
 class Solution {
 
-    private int n;
-    private int m;
-    private char[][] map;
-    private int k;
-    private int r;
-    private int c;
-    private int[][] dir = { {1, 0}, {0, -1}, {0, 1}, {-1, 0}};
-    private String[] dirStr = {"d", "l", "r", "u"};
-
-    public String solution(int n1, int m1, int x, int y, int r1, int c1, int k1) {
-        map = new char[n1 + 1][m1 + 1];
-        n = n1;
-        m = m1;
-        k = k1;
-        r = r1;
-        c = c1;
-
-        // 남은 거리가 도착지까지 도착하기 위한 최소 거리보다 적으면 안됨
+    // 사전 순으로 해당 차례에 어디 방향으로 갈지 결정한다. 사전 순이기 때문에 현재 탐색하고 있는 방향으로 도착지까지 갈 수 있다면 무조건 이 방향을 선택하는 게 맞다.
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
         if(distance(x, y, r, c) > k) return "impossible";
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-                if(i == x && j == y) map[i][j] = 'S';
-                else if(i == r && j == c) map[i][j] = 'E';
-                else map[i][j] = '.';
+        StringBuilder sb = new StringBuilder();
+        for(int i = k - 1; i >= 0; i --) {
+            if(x + 1 <= n && distance(x + 1, y, r, c) <= i) {
+                x ++;
+                sb.append("d");
+            } else if(y - 1 > 0 && distance(x, y - 1, r, c) <= i) {
+                y --;
+                sb.append("l");
+            } else if(y + 1 <= m && distance(x, y + 1, r, c) <= i) {
+                y ++;
+                sb.append("r");
+            } else if(x - 1 > 0 && distance(x - 1, y, r, c) <= i) {
+                x --;
+                sb.append("u");
+            } else if(i == 0) { // 남은 거리가 없는데 갈 방향도 없다면 불가능
+                return "impossible";
             }
         }
 
-        return bfs(x, y);
-    }
-
-    private String bfs(int sx, int sy) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(sx, sy, new StringBuilder()));
-
-        while(!pq.isEmpty()) {
-            Node curr = pq.remove();
-
-            // System.out.println(curr.x + " " + curr.y + " " + curr.route);
-            for(int i = 0; i < 4; i++) {
-                int nx = curr.x + dir[i][0];
-                int ny = curr.y + dir[i][1];
-                if(nx <= 0 || nx > n || ny <= 0 || ny > m || curr.route.toString().length() == k) continue;
-                if(distance(nx, ny, r, c) > k - curr.route.length() - 1) continue;
-                if(map[nx][ny] == 'E' && curr.route.length() + 1 == k) return curr.route.append(dirStr[i]).toString();
-                StringBuilder sb = new StringBuilder(curr.route.toString());
-                pq.add(new Node(nx, ny, sb.append(dirStr[i])));
-                break;
-            }
-        }
-
-        return "impossible";
+        return sb.toString();
     }
 
     private int distance(int sx, int sy, int ex, int ey) {
         return Math.abs(sx - ex) + Math.abs(sy - ey);
-    }
-
-    private class Node implements Comparable<Node> {
-        private int x;
-        private int y;
-        private StringBuilder route;
-
-        Node(int x, int y, StringBuilder route) {
-            this.x = x;
-            this.y = y;
-            this.route = route;
-        }
-
-        @Override
-        public int compareTo(Node n) {
-            if(this.route.length() != n.route.length()) {
-                return n.route.length() - this.route.length();
-            }
-            return this.route.toString().compareTo(n.route.toString());
-        }
     }
 }
