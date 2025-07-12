3f4b4c37df3144abf1b3bbf8ae1cc4744a657b9b
suzzing-V <84590065+suzzingv@users.noreply.github.com>
2025-07-12 16:20:06 +0900

    dp

--- p코딩테스트공부/Solution.java
+++ p코딩테스트공부/Solution.java
@@ -2,43 +2,61 @@ import java.util.*;
 
 class Solution {
 
+    // 알고력 i, 코딩력 j을 만드는 데 드는 최소 비용
     private int[][] dp;
-    private int talp = 0;
-    private int tcop = 0;
+    private int n = 0;
+    private int m = 0;
 
     public int solution(int alp, int cop, int[][] problems) {
-        // 도달해야하는 알고력과 코딩력 구하기
+
+        // 목표점수 구하기
         for(int i = 0; i < problems.length; i++) {
-            talp = Math.max(problems[i][0], talp);
-            tcop = Math.max(problems[i][1], tcop);
+            n = Math.max(problems[i][0], n);
+            m = Math.max(problems[i][1], m);
         }
 
-        dp = new int[talp + 1][tcop + 1];
-        for(int i = 0; i < dp.length; i++) {
+        // 목표점수가 초기 점수보다 작으면 이미 목표 달성한 것이므로 0 리턴
+        if(n <= alp && m <= cop) return 0;
+
+        // 알고력의 초기점수가 목표점수보다 크면 초기점수를 목표점수로 설정. 이렇게 안하면 초기점수의 코스트 초기화할 수 없음
+        if(alp > n) n = alp;
+        if(cop > m) m = cop;
+
+        dp = new int[n + 1][m + 1];
+        for(int i = 0; i <= n; i++) {
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
 
-        // 초기 코딩력과 초기 알고력이 타겟값보다 큰 경우 타겟값으로 보정. 넘었을 때 타겟값은 충족하는 것이므로 타겟값까지의 걸리는 시간이다.
-        alp = Math.min(alp, talp);
-        cop = Math.min(cop, tcop);
-        dp[alp][cop] = 0; // 초기 알고력, 초기 코딩력에 도달하기 위해 필요한 시간은 0.
+        // System.out.println(n + " " + m);
+        dp[alp][cop] = 0;
 
-        for(int i = alp; i <= talp; i++) {
-            for(int j = cop; j <= tcop; j++) {
-                // 트레이닝. 알고력, 코딩력 +1 이 범위 안인 경우 갱신.
-                if(i + 1 <= talp) dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + 1);
-                if(j + 1 <= tcop) dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + 1);
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                if(dp[i][j] == Integer.MAX_VALUE) continue;
 
-                // 문제 풀기
+                // 공부는 보정 안해도 되는 이유: 어떤 경우라도 dp[n][j]보다 dp[n][j] + 1보다 작으므로 비교하는 게 의미가 없다.
+                // 알고력 공부
+                if(i + 1 <= n) dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + 1);
+                // 코딩력 공부
+                if(j + 1 <= m) dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + 1);
+                // 문제 해결
                 for(int k = 0; k < problems.length; k++) {
-                    if(problems[k][0] <= i && problems[k][1] <= j) { // 현재 상태에서 풀 수 있는 문제 선별
-                        int newAlp = Math.min(talp, i + problems[k][2]);
-                        int newCop = Math.min(tcop, j + problems[k][3]); // 타겟 넘는 값 보정. 타겟값보다 더 크면 타겟값을 충족한다는 뜻이므로 타겟값의 시간 갱신.
-                        dp[newAlp][newCop] = Math.min(dp[newAlp][newCop], dp[i][j] + problems[k][4]);
+                    int alg_req = problems[k][0];
+                    int cop_req = problems[k][1];
+                    int alg_rwd = problems[k][2];
+                    int cop_rwd = problems[k][3];
+                    int cost = problems[k][4];
+                    // 알고력과 코딩력이 지금 점수보다 낮거나 같아야 풀 수 있다
+                    if(alg_req <= i && cop_req <= j) {
+                        // 목표점수보다 더 높은 점수 얻을 수 있는 경우도 목표를 달성한 것이므로 이 경우도 고려한다.
+                        int x = Math.min(i + alg_rwd, n);
+                        int y = Math.min(j + cop_rwd, m);
+                        dp[x][y] = Math.min(dp[x][y], dp[i][j] + cost);
                     }
                 }
             }
         }
-        return dp[talp][tcop];
+
+        return dp[n][m];
     }
 }
