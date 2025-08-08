42d16efccc625a0b7bafc5935143b48fda6f6dcd
suzzing-V <sjlim1999@gmail.com>
2025-08-08 14:06:45 +0900

    bfs

--- p양과늑대/Solution.java
+++ p양과늑대/Solution.java
@@ -1,63 +1,65 @@
+// 시간 복잡도: 17 * 17
+// 각 인접노드를 방문했을 때의 상황마다 큐에 넣는다. 가능한 경우만 넣는다.
+
 import java.util.*;
 
-// 현재 노드에서 갈 수 있는 노드는 자식노드와 이전노드의 인접한 노드들. 탐색하다가 멈추고 다른 거 보기.
 class Solution {
 
-    private List<Integer>[] conn;
     private Queue<Node> queue = new LinkedList<>();
-    private int max = 0;
-    private int[] info;
+    private List<Integer>[] conn;
+    private int answer = 0;
 
-    private class Node {
-        private int curr;
-        private int sheep;
-        private int wolf;
-        private List<Integer> next;
-
-        private Node(int curr, int sheep, int wolf, List<Integer> next) {
-            this.curr = curr;
-            this.sheep = sheep;
-            this.wolf = wolf;
-            this.next = next;
-        }
-    }
-
-
-    public int solution(int[] info1, int[][] edges) {
-        info = info1;
+    public int solution(int[] info, int[][] edges) {
         conn = new ArrayList[info.length];
         for(int i = 0; i < conn.length; i++) {
             conn[i] = new ArrayList<>();
         }
-        for(int i = 0; i < edges.length; i++) { // 노드 관계 저장
+
+        for(int i = 0; i < edges.length; i++) {
             conn[edges[i][0]].add(edges[i][1]);
         }
 
-        List<Integer> initNext = new ArrayList<>();
-        for(int son : conn[0]) {
-            initNext.add(son);
-        }
+        List<Integer> init = new ArrayList<>();
+        init.addAll(conn[0]);
+        queue.add(new Node(1, 0, 0, init));
 
-        queue.add(new Node(0, 1, 0, initNext));
         while(!queue.isEmpty()) {
             Node curr = queue.remove();
-            if(curr.sheep == curr.wolf) continue; // 늑대와 양 수가 같으면 더 탐색 불가
 
-            max = Math.max(max, curr.sheep); // 최대값 갱신
-            for(int next : curr.next) { // 인접 리스트 돌면서 다음 거 탐색.
-                List<Integer> nextList = new ArrayList<>();
-                for(int i : conn[next]) { // 자식 노드 추가
-                    nextList.add(i);
-                }
-                for(int i : curr.next) { // 현재 노드에 인접한 노드들 추가
-                    if(next == i) continue;
-                    nextList.add(i);
-                }
+            answer = Math.max(answer, curr.sheep);
 
-                if(info[next] == 0) queue.add(new Node(next, curr.sheep + 1, curr.wolf, nextList));
-                else queue.add(new Node(next, curr.sheep, curr.wolf + 1, nextList));
+            // 인접 노드들 중 갈 수 있는 노드 큐에 넣기
+            for(int next : curr.near) {
+                int ns = curr.sheep + (info[next] == 0? 1 : 0);
+                int nw = curr.wolf + (info[next] == 1? 1: 0);
+
+                // 다음 노드로 갈 경우 양의 개수가 늑대의 개수보다 작거나 같으면 못감
+                if(ns <= nw) continue;
+
+                List<Integer> nl = new ArrayList<>();
+                nl.addAll(curr.near);
+                // 현재노드는 제거
+                nl.remove((Integer)next);
+                // 현재 노드의 자식 노드들 넣기
+                nl.addAll(conn[next]);
+
+                queue.add(new Node(ns, nw, next, nl));
             }
         }
-        return max;
+        return answer;
+    }
+
+    private class Node {
+        int sheep; // num에서 양의 개수
+        int wolf; // num에서 늑대의 개수
+        List<Integer> near; // 현재 상태에서 인접 노드들
+        int num;
+
+        Node(int sheep, int wolf, int num, List<Integer> near) {
+            this.sheep = sheep;
+            this.wolf = wolf;
+            this.num = num;
+            this.near = near;
+        }
     }
 }
