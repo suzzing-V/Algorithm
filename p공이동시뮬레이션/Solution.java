9ecdb27ddd42a175bdcf057bf9d8de6e3d0282e0
suzzing-V <sjlim1999@gmail.com>
2025-08-11 22:03:22 +0900

    implementation

--- p공이동시뮬레이션/Solution.java
+++ p공이동시뮬레이션/Solution.java
@@ -1,50 +1,47 @@
+import java.util.*;
+
+// 쿼리를 거꾸로 돌면서 현재 범위 상태에 도달할 수 있는 이전 범위 상태를 구한다. 이전 상태에서 현재 상태로 밀리는 방향으로 더이상 밀 수 없을 때는 이전 범위가 현재 범위에서 늘어난다. 밀 수 있을 때는 그냥 범위를 이동만 시킨다.
+// 시간 복잡도: O(n)
 class Solution {
     public long solution(int n, int m, int x, int y, int[][] queries) {
-        long x1 = x; // 좌상단
-        long x2 = x; // 우하단
-        long y1 = y; // 좌상단
-        long y2 = y; // 우하단
+        int x1 = x;
+        int x2 = x;
+        int y1 = y;
+        int y2 = y;
+
         for(int i = queries.length - 1; i >= 0; i--) {
             int type = queries[i][0];
-            int d = queries[i][1];
-            if(type == 0) {
-                if(y1 == 0) { // 전 위치에서 현재 위치로 이동 방향이 벽이면 그 범위 안의 위치가 다 가능하므로 늘린다.
-                    y2 += d;
-                } else { // 벽이 아니면 한 위치만 가능하므로 이동한다.
-                    y1 += d;
-                    y2 += d;
-                }
+            int dx = queries[i][1];
+
+            if(type == 0 && y1 == 0) {
+                y2 = Math.min(m - 1, y2 + dx);
+            } else if(type == 1 && y2 == m - 1) {
+                y1 = Math.max(0, y1 - dx);
+            } else if(type == 2 && x1 == 0) {
+                x2 = Math.min(n - 1, x2 + dx);
+            } else if(type == 3 && x2 == n - 1) {
+                x1 = Math.max(0, x1 - dx);
+            } else if(type == 0) {
+                if(y1 + dx >= m) return 0;
+                y1 = y1 + dx;
+                y2 = Math.min(y2 + dx, m - 1);
             } else if(type == 1) {
-                if(y2 == m - 1) {
-                    y1 -= d;
-                } else {
-                    y1 -= d;
-                    y2 -= d;
-                }
+                if(y2 - dx < 0) return 0;
+                y2 = y2 - dx;
+                y1 = Math.max(0, y1 - dx);
             } else if(type == 2) {
-                if(x1 == 0) {
-                    x2 += d;
-                } else {
-                    x1 += d;
-                    x2 += d;
-                }
+                if(x1 + dx >= n) return 0;
+                x1 = x1 + dx;
+                x2 = Math.min(x2 + dx, n - 1);
             } else if(type == 3) {
-                if(x2 == n - 1) {
-                    x1 -= d;
-                } else {
-                    x1 -= d;
-                    x2 -= d;
-                }
+                if(x2 - dx < 0) return 0;
+                x2 = x2 - dx;
+                x1 = Math.max(0, x1 - dx);
             }
-
-            if(x1 >= n || y1 >= m || x2 < 0 || y2 < 0) return 0; // 사각형 통째로 격자 나가면 가능한 위치가 없다.
-
-            x1 = Math.max(0, x1);
-            y1 = Math.max(0, y1);
-            x2 = Math.min(n - 1, x2);
-            y2 = Math.min(m - 1, y2); // 범위 넘어갈 경우 조정 필요
+            // System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
         }
-        long answer = (x2 - x1 + 1) * (y2 - y1 + 1);
+
+        long answer = (long)(x2 - x1 + 1) * (long)(y2 - y1 + 1);
         return answer;
     }
 }
