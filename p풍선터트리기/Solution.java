938afbf56cf780e07414956d0284607272750f80
suzzing-V <84590065+suzzingv@users.noreply.github.com>
2025-08-22 10:33:28 +0900

    누적합

--- p풍선터트리기/Solution.java
+++ p풍선터트리기/Solution.java
@@ -1,33 +1,46 @@
 import java.util.*;
 
+// 작은 값 터뜨리는 건 마지막에 쓴다. 그전까지는 큰값만 터뜨리므로 남아있는 건 가장 작은 값아다.
+// 검사하려는 값 기준 왼쪽과 오른쪽 구간에서 가장 큰 값을 찾고, 그 수들이 검사하려는 값보다 둘다 작으면 어떻게 해도 검사값을 남길 수 없다.
+// 시간복잡도: O(n)
 class Solution {
 
-    private int[] left_min; // 0 ~ i까지의 수 중 최소값
-    private int[] right_min; // i ~ a.length - 1까지의 수 중 최소값
+    private int[] left_min;
+    private int[] right_min;
+    private int n;
 
     public int solution(int[] a) {
-        left_min = new int[a.length];
-        right_min = new int[a.length];
-
-        if(a.length == 2) return 1;
-
-        int cnt = 2; // 첫번째 수, 마지막 수는 무조건 가능
-
-        // 양쪽 구간 최소값 구하기
+        n = a.length;
+        left_min = new int[n];
+        right_min = new int[n];
         left_min[0] = a[0];
-        right_min[a.length - 1] = a[a.length - 1];
-        for(int i = 1; i < a.length; i++) {
-            left_min[i] = Math.min(a[i], left_min[i - 1]);
+        right_min[n - 1] = a[n - 1];
+
+        //왼쪽 구간에서 가장 작은 값 구하기
+        for(int i = 1; i < n; i++) {
+            left_min[i] = Math.min(left_min[i - 1], a[i]);
         }
-        for(int i = a.length - 2; i >= 0; i--) {
-            right_min[i] = Math.min(a[i], right_min[i + 1]);
+        //오른쪽 구간에서 가장 작은 값 구하기
+        for(int i = n - 2; i >= 0; i --) {
+            right_min[i] = Math.min(right_min[i + 1], a[i]);
         }
 
-        // 왼쪽 최소값, 오른쪽 최소값, 현재 값 비교해서 현재 값이 제일 크면 최후의 풍선 될 수 없다.
-        for(int i = 1; i < a.length - 1; i++) {
-            if((left_min[i - 1] < a[i] && right_min[i + 1] < a[i])) continue;
+        // 특정 값에서 양쪽에서의 최솟값이 특정값보다 둘다 작으면 그 수는 마지막까지 남을 수 없다.
+        // 양쪽값이 하나밖에 없다면 무조건 가능
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(i == 0 || i == n - 1) {
+                cnt ++;
+                continue;
+            }
+
+            int left = left_min[i - 1];
+            int right = right_min[i + 1];
+
+            if(left < a[i] && right < a[i]) continue;
             cnt ++;
         }
+
         return cnt;
     }
 }
