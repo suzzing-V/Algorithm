5fac5a3241edfadffe934b3394b4bf4132059c56
suzzing-V <84590065+suzzingv@users.noreply.github.com>
2025-08-26 10:39:49 +0900

    two pointer

--- p보석쇼핑/Solution.java
+++ p보석쇼핑/Solution.java
@@ -1,40 +1,49 @@
 import java.util.*;
 
+// 시간복잡도: 10^5
 class Solution {
-    private static Set<String> set = new HashSet<>();
-    private static Map<String, Integer> jewels = new HashMap<>();
 
-    public int[] solution(String[] gems) {
-        int left = 0;
-        int right = 0;
-        for(int i = 0; i < gems.length; i++) { // 보석 종류 세기
-            set.add(gems[i]);
-        }
+    private String[] gems;
+    private Map<String, Integer> counts = new HashMap<>();
+    private int n;
+    private int min = Integer.MAX_VALUE;
+    private int start = 0;
+    private int end = 0;
 
-        int[] answer = {0, gems.length - 1};
-
-        int kind = set.size();
-        jewels.put(gems[0], 1);
-        while(true) {
-            if(jewels.size() == kind) { // 현재 선택한 구간 안의 보석 수가 보석 종류와 같으면 길이가 최소값일 경우 갱신하고 왼쪽 포인터 +1
-                // System.out.println(left + " "+ right);
-                if(answer[1] - answer[0] > right - left) {
-                    answer[1] = right;
-                    answer[0] = left;
-                }
-
-                if(jewels.get(gems[left]) == 1) jewels.remove(gems[left]);
-                else jewels.put(gems[left], jewels.get(gems[left]) - 1);
-                left ++;
-            } else { // 충족되지 않으면 오른쪽 포인터 늘리기
-                right ++;
-                if(right == gems.length) break; // 오른쪽 포인터가 범위 넘었다는 건 그때까지 충족하지 못했다는 뜻이다. 아무리 왼쪽 포인터를 조정해도 충족할 수 없으므로 멈춘다.
-                jewels.put(gems[right], jewels.getOrDefault(gems[right], 0) + 1);
+    public int[] solution(String[] gems1) {
+        gems = gems1;
+        n = gems.length;
+        for(int i = 0; i < n; i++) {
+            if(counts.get(gems[i]) == null) {
+                counts.put(gems[i], 0);
             }
         }
 
-        answer[0] ++;
-        answer[1] ++;
+        // 전체 보석 종류 수
+        int total = counts.size();
+        int left = 0;
+        int right = 0;
+        int cnt = 1;
+        counts.put(gems[0], 1);
+        while(right < n) {
+            if(cnt == total) {
+                if(min > right - left) {
+                    min = right - left;
+                    start = left;
+                    end = right;
+                }
+                counts.put(gems[left], counts.get(gems[left]) - 1);
+                if(counts.get(gems[left]) == 0) cnt --;
+                left ++;
+            } else {
+                right ++;
+                if(right >= n) break;
+                counts.put(gems[right], counts.get(gems[right]) + 1);
+                if(counts.get(gems[right]) == 1) cnt ++;
+            }
+        }
+
+        int[] answer = {start + 1, end + 1};
         return answer;
     }
 }
