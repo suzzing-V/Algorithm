6c75e917a63a76598f4235f1037e3793ad53cfe3
suzzing-V <84590065+suzzingv@users.noreply.github.com>
2025-09-01 14:23:06 +0900

    dfs

--- p외벽점검/Solution.java
+++ p외벽점검/Solution.java
@@ -1,68 +1,68 @@
 import java.util.*;
 
+// 시간복잡도: 이론상 15^8이지만 프루닝 적용해서 줄임
+// 가장 탐색 거리가 긴 친구부터 특정 weak에 두면서 이 친구의 범위 안에 있는 weak들을 삭제시키고 다음 dfs로 넘어간다.
+// 이 친구의 범위 시작점이 start고, end가 start + end일 때, end가 끝부분을 넘어간다면 범위 안의 수는 start보다 크고, end % n보다 작아야 한다.
+// 넘어가지 않는다면 start보다 크고, end보다 작으면 된다.
 class Solution {
 
-    private int min = Integer.MAX_VALUE;
-    private int[] dist;
-    private int[] weak;
+    private int min = 10;
+    private Integer[] dist;
     private int n;
 
-    public int solution(int n1, int[] weak1, int[] dist1) {
-        dist = dist1;
-        n = n1;
-        // 제일 거리가 긴 친구부터 배치하기 위해 정렬한다. 최대한 거리가 긴 친구부터 배치하는 것이 무조건 최소 인원을 구하는 방법이다.
-        Arrays.sort(dist);
-        weak = weak1;
-        List<Integer> curr_weak = new ArrayList<>();
-        for(int i = 0; i < weak.length; i++) {
-            curr_weak.add(weak[i]);
+    public int solution(int n1, int[] weak, int[] dist1) {
+        dist = new Integer[dist1.length];
+        for(int i = 0; i < dist1.length; i++) {
+            dist[i] = (Integer) dist1[i];
         }
-        // 반시계방향은 고려할 필요가 없다. 어짜피 시계방향으로 모두 탐색하면서 시계방향일 때 시작점인 경우와 반시계방향일 때 끝점인 경우가 중복되기 때문이다.
-        dfs(dist.length - 1, curr_weak, 0);
+        n = n1;
+        // 제일 거리가 긴 친구부터 투입해야 유리하므로 가지치기를 빨리할 수 있어 탐색 시간을 줄일 수 있다.
+        Arrays.sort(dist, Collections.reverseOrder());
 
-        if(min == Integer.MAX_VALUE) min = -1;
+        List<Integer> rest = new ArrayList<>();
+        for(int i = 0; i < weak.length; i++) {
+            rest.add(weak[i]);
+        }
+        dfs(0, rest);
+
+        if(min == 10) return -1;
         return min;
     }
 
-    private void dfs(int d_idx, List<Integer> curr_weak, int cnt) {
-        if(curr_weak.isEmpty()) { // 취약점 모두 제거했으면 최소값 갱신
-            min = Math.min(cnt, min);
-            return;
-        }
-        if(d_idx < 0) { // 취약점 아직 남아있는데 친구 다 썼으면 이 루트로는 취약점 모두 제거할 수 없다.
+    private void dfs(int idx, List<Integer> rest) {
+        // 이미최솟값에 저장된 친구 수가 더 작으면 어짜피 친구 더쓸일밖에 없기 때문에 탐색할 필요가 없다
+        if(min <= idx) {
             return;
         }
 
-        int m = dist[d_idx]; // 현재 친구의 거리
+        // 외벽 다 삭제함 -> 끝
+        if(rest.size() == 0) {
+            min = Math.min(idx, min);
+            return;
+        }
 
-        // 시계방향
-        // 현재 친구를 남은 취약점을 시작점으로 다 배치해본다.
-        for(int i = 0; i < curr_weak.size(); i++) {
-            List<Integer> rest_weak = new ArrayList<>(curr_weak);
-            int end = curr_weak.get(i) + m; // 현재 친구가 도달할 수 있는 끝점
-            int rest = -1; // 분기점을 넘어갈 경우 남은 거 처리 위함.
-            if(end >= n) { // 넘어갈 경우 분기점까지 없애고 남은 거리
-                rest = end - n;
-                end = n - 1;
-            }
+        // 친구 다 썼는데 외벽 남으면 갱신 안하고 끝
+        if(idx == dist.length) {
+            return;
+        }
 
-            int j = i;
-            while(true) { // 취약점 삭제
-                if(j >= rest_weak.size() || rest_weak.get(j) > end) {
-                    break;
-                }
-                rest_weak.remove(j);
-            }
-            if(rest != -1) { // 분기점까지 다 삭제하고 남은 경우 분기점부터 다시 삭제
-                j = 0;
-                while(true) {
-                    if(j >= rest_weak.size() || rest_weak.get(j) > rest) {
-                        break;
-                    }
-                    rest_weak.remove(j);
+        int range = dist[idx];
+        for(int i = 0; i < rest.size(); i++) {
+            int start = rest.get(i);
+            int end = start + range;
+
+            List<Integer> next_rest = new ArrayList<>(rest);
+            for(int j = 0; j < next_rest.size(); j++) {
+                // 범위가 끝을 안 넘어갈 경우 start~end 사이의 값이면 삭제
+                if(end < n && start <= next_rest.get(j) && next_rest.get(j) <= end) {
+                    next_rest.remove(j--);
+                    // 범위가 끝을 넘어갈 경우 start보다 같거나 크거나, end % n보다 작거나 같으면 범위 안에 있는 것
+                } else if(end >= n && (start <= next_rest.get(j) || next_rest.get(j) <= end % n)) {
+                    next_rest.remove(j--);
                 }
             }
-            dfs(d_idx - 1, rest_weak, cnt + 1);
+            // System.out.println(start + " " + end + " " + next_rest.toString());
+            dfs(idx + 1, next_rest);
         }
     }
 }
