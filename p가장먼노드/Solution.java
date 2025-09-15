617163885014813cb2917472861f3fefe83315ed
suzzing-V <84590065+suzzingv@users.noreply.github.com>
2025-09-15 17:03:35 +0900

    dikstra

--- p가장먼노드/Solution.java
+++ p가장먼노드/Solution.java
@@ -1,51 +1,39 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
 
     private int[] dist;
-    private List<Integer>[] lines;
-
-    private class Node implements Comparable<Node> {
-
-        private int num;
-        private int d;
-
-        private Node(int num, int d) {
-            this.num = num;
-            this.d = d;
-        }
-
-        @Override
-        public int compareTo(Node n) {
-            return this.d - n.d;
-        }
-    }
+    private ArrayList<Node>[] conn;
 
     public int solution(int n, int[][] edge) {
         dist = new int[n + 1];
-        lines = new ArrayList[n + 1];
-        Arrays.fill(dist, 30000);
-        for(int i = 0; i <= n; i++) {
-            lines[i] = new ArrayList<>();
+        conn = new ArrayList[n + 1];
+        for(int i = 1; i <= n; i++) {
+            conn[i] = new ArrayList<>();
+            dist[i] = Integer.MAX_VALUE;
         }
 
         for(int i = 0; i < edge.length; i++) {
-            lines[edge[i][0]].add(edge[i][1]);
-            lines[edge[i][1]].add(edge[i][0]);
+            conn[edge[i][0]].add(new Node(edge[i][1], 0));
+            conn[edge[i][1]].add(new Node(edge[i][0], 0));
         }
 
+        dist[1] = 0;
         dikstra();
 
         int max = 0;
+        for(int i = 1; i <= n; i++) {
+            max = Math.max(max, dist[i]);
+        }
+
         int cnt = 0;
-        for(int i = 2; i <= n; i++) {
-            if(max < dist[i]) {
-                max = dist[i];
-                cnt = 1;
-            } else if(max == dist[i]) {
+        for(int i = 1; i <= n; i++) {
+            if(dist[i] == max) {
                 cnt ++;
             }
         }
+
         return cnt;
     }
 
@@ -56,14 +44,29 @@ class Solution {
         while(!pq.isEmpty()) {
             Node curr = pq.remove();
 
-            if(dist[curr.num] < curr.d) continue;
-            for(int i = 0; i < lines[curr.num].size(); i++) {
-                int next = lines[curr.num].get(i);
-                if(dist[next] > curr.d + 1) {
-                    dist[next] = curr.d + 1;
-                    pq.add(new Node(next, curr.d + 1));
+            if(dist[curr.n] < curr.d) continue;
+
+            for(Node node : conn[curr.n]) {
+                if(dist[node.n] > curr.d + 1) {
+                    dist[node.n] = curr.d + 1;
+                    pq.add(new Node(node.n, curr.d + 1));
                 }
             }
+        }
+    }
+
+    private class Node implements Comparable<Node> {
+        int n;
+        int d;
+
+        Node(int n, int d) {
+            this.n = n;
+            this.d = d;
+        }
+
+        @Override
+        public int compareTo(Node n) {
+            return this.d - n.d;
         }
     }
 }
