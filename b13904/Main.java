9affb45f7e565edf31babc531d3cf1e9f46fde33
suzzing-V <84590065+suzzingv@users.noreply.github.com>
2025-09-19 18:12:09 +0900

    sfd

--- b13904/Main.java
+++ b13904/Main.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int n;
+    private static Stack<Task>[] tasks = new Stack[1001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        for(int i = 1; i <= 1000; i++) {
+            tasks[i] = new Stack<>();
+        }
+
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int d = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+
+            tasks[d].add(new Task(s, d));
+            max = Math.max(max, d);
+        }
+
+        PriorityQueue<Task> possible = new PriorityQueue<>();
+        int sum = 0;
+        for(int i = max; i >= 1; i --) {
+            while(!tasks[i].isEmpty()) {
+                possible.add(tasks[i].pop());
+            }
+
+            if(!possible.isEmpty()) {
+                sum += possible.remove().s;
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    private static class Task implements Comparable<Task> {
+        int d;
+        int s;
+
+        Task(int s, int d) {
+            this.d = d;
+            this.s = s;
+        }
+
+        @Override
+        public int compareTo(Task t) {
+            return t.s - this.s;
+        }
+    }
 }
--- b2309/Main.java
+++ b2309/Main.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int[] heights = new int[9];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 0; i < 9; i ++) {
+            heights[i] = Integer.parseInt(bf.readLine());
+        }
+        make100(0, 0, new ArrayList<>());
+    }
+
+    private static boolean make100(int idx, int sum, List<Integer> selected) {
+
+        if(sum == 100 && selected.size() == 7) {
+            printResult(selected);
+            return true;
+        }
+        if(idx >= 9) return false;
+
+            sum += heights[idx];
+            selected.add(heights[idx]);
+            if(make100(idx + 1, sum, selected)) {
+                return true;
+            }
+            sum -= heights[idx];
+            selected.remove(selected.size() - 1);
+            if(make100(idx + 1, sum, selected)) {
+                return true;
+            }
+        return false;
+    }
+
+    private static void printResult(List<Integer> selected) {
+        Collections.sort(selected);
+        for(int h : selected) {
+            System.out.println(h);
+        }
+    }
 }
